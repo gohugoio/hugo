8c03141307a375709d4f9ae4a765ca1935b72051
Noah Campbell <noahcampbell@gmail.com>
2013-08-12 13:57:47 -0700

    Use / for template names regardless of platform.
    The path seperator was causing templates to not be loaded on windows.
    Now all template names use / internally.

--- hugolib/path_seperators_test.go
+++ hugolib/path_seperators_test.go
@@ -23,25 +23,29 @@ func TestDegenerateMissingFolderInPageFilename(t *testing.T) {
 }
 
 func TestNewPageWithFilePath(t *testing.T) {
-	toCheck := []map[string]string{
-		{"input": filepath.Join("sub", "foobar.html"), "expect": "sub"},
-		{"input": filepath.Join("content", "sub", "foobar.html"), "expect": "sub"},
-		{"input": filepath.Join("content", "dub", "sub", "foobar.html"), "expect": "sub"},
+	toCheck := []struct{
+		input string
+		section string
+		layout string
+	}{
+		{filepath.Join("sub", "foobar.html"), "sub", "sub/single.html"},
+		{filepath.Join("content", "sub", "foobar.html"), "sub", "sub/single.html"},
+		{filepath.Join("content", "dub", "sub", "foobar.html"), "sub", "sub/single.html"},
 	}
 
 	for _, el := range toCheck {
-		p, err := ReadFrom(strings.NewReader(SIMPLE_PAGE_YAML), el["input"])
+		p, err := ReadFrom(strings.NewReader(SIMPLE_PAGE_YAML), el.input)
 		if err != nil {
 			t.Fatalf("Reading from SIMPLE_PAGE_YAML resulted in an error: %s", err)
 		}
-		if p.Section != el["expect"] {
-			t.Fatalf("Section not set to %s for page %s. Got: %s", el["expect"], el["input"], p.Section)
+		if p.Section != el.section {
+			t.Fatalf("Section not set to %s for page %s. Got: %s", el.section, el.input, p.Section)
+		}
+
+		if p.Layout() != el.layout {
+			t.Fatalf("Layout incorrect. Expected: '%s', Got: '%s'", el.layout, p.Layout())
 		}
 	}
 }
 
-func TestSettingOutFileOnPageContainsCorrectSlashes(t *testing.T) {
-	s := &Site{Config: Config{}}
-	p := NewPage(filepath.Join("sub", "foobar"))
-	s.setOutFile(p)
-}
+
