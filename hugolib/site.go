023567b05e1542cbdd25514f2ba7dd15a6e3a6c8
elij <elij@wundrd.net>
2013-08-12 19:01:23 -0700

    fix bad tab/space due to paste

--- hugolib/site.go
+++ hugolib/site.go
@@ -470,11 +470,11 @@ func (s *Site) RenderLists() error {
 
 		if a := s.Tmpl.Lookup("rss.xml"); a != nil {
 			// XML Feed
-            if s.Config.UglyUrls {
-                    n.Url = Urlize(section + ".xml")
-            } else {
-                    n.Url = Urlize(section + "/" + "index.xml")
-            }
+			if s.Config.UglyUrls {
+			        n.Url = Urlize(section + ".xml")
+			} else {
+			        n.Url = Urlize(section + "/" + "index.xml")
+			}
 			n.Permalink = template.HTML(string(n.Site.BaseUrl) + n.Url)
 			y := s.NewXMLBuffer()
 			s.Tmpl.ExecuteTemplate(y, "rss.xml", n)
