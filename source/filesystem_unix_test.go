8df88496e24c80298ff8d8d1d36a67974e56a7e2
Noah Campbell <noahcampbell@gmail.com>
2013-09-20 20:05:20 -0700

    Fix breaking test cases on unix platform
    Rename platform parameter file to agnostic name. Both darwin and linux
    can share the same paramters.

--- source/filesystem_unix_test.go
+++ source/filesystem_unix_test.go
@@ -1,13 +1,14 @@
+// +build linux,darwin
 package source
 
 //
 // NOTE, any changes here need to be reflected in filesystem_windows_test.go
 //
-var platformBase = "foo/bar/boo/"
+var platformBase = "/base/"
 var platformPaths = []TestPath{
 	{"foobar", "foobar", "aaa", "", ""},
 	{"b/1file", "1file", "aaa", "b", "b/"},
 	{"c/d/2file", "2file", "aaa", "d", "c/d/"},
-	{"/e/f/3file", "3file", "aaa", "f", "e/f/"},
-	{"section\\foo.rss", "foo.rss", "aaa", "section", "section/"},
+	{"/base/e/f/3file", "3file", "aaa", "f", "e/f/"},
+	{"section/foo.rss", "foo.rss", "aaa", "section", "section/"},
 }
