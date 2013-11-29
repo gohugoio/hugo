950d9f55a53705b332a919df1a2f45307019d2fd
Ruben <ruben.nellen@gmail.com>
2013-11-29 08:22:44 +0100

    Testcases for multilevel structure were wrong

--- source/filesystem_unix_test.go
+++ source/filesystem_unix_test.go
@@ -9,7 +9,7 @@ var platformBase = "/base/"
 var platformPaths = []TestPath{
 	{"foobar", "foobar", "aaa", "", ""},
 	{"b/1file", "1file", "aaa", "b", "b/"},
-	{"c/d/2file", "2file", "aaa", "d", "c/d/"},
-	{"/base/e/f/3file", "3file", "aaa", "f", "e/f/"},
+	{"c/d/2file", "2file", "aaa", "c", "c/d/"},
+	{"/base/e/f/3file", "3file", "aaa", "e", "e/f/"},
 	{"section/foo.rss", "foo.rss", "aaa", "section", "section/"},
 }
