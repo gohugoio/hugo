7a937aaf39682e557b1bc6ed9ac4345cdfebcb63
Niels Widger <niels.widger@gmail.com>
2014-01-30 17:50:47 -0500

    Add back blackfriday extensions during Markdown conversion
    Modified markdownRender and markdownRenderWithTOC in hugolib/page.go to
    use the same flags and extensions as were previously used when we simply
    called blackfriday.MarkdownCommon to convert Markdown to HTML.  These
    flags/extensions were dropped during the refactor that added the
    `.TableOfContents` page variable, and caused features like Markdown
    tables to no longer work.
    
    Modified the expected output for TestTableOfContents in page_test.go,
    apparently changing the flags/extensions caused an `&mdash;` to become
    `&ndash;`.

--- hugolib/page.go
+++ hugolib/page.go
@@ -587,20 +587,42 @@ func (page *Page) Convert() error {
 func markdownRender(content []byte) []byte {
 	htmlFlags := 0
 	htmlFlags |= blackfriday.HTML_SKIP_SCRIPT
+	htmlFlags |= blackfriday.HTML_USE_XHTML
 	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
 	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
 
-	return blackfriday.Markdown(content, renderer, 0)
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+
+	return blackfriday.Markdown(content, renderer, extensions)
 }
 
 func markdownRenderWithTOC(content []byte) []byte {
 	htmlFlags := 0
 	htmlFlags |= blackfriday.HTML_SKIP_SCRIPT
 	htmlFlags |= blackfriday.HTML_TOC
+	htmlFlags |= blackfriday.HTML_USE_XHTML
 	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
 	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
 
-	return blackfriday.Markdown(content, renderer, 0)
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+
+	return blackfriday.Markdown(content, renderer, extensions)
 }
 
 func extractTOC(content []byte) (newcontent []byte, toc []byte) {
