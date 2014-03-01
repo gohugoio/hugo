0a775650b54b07f5a3c149c3f6a441d8f4befda7
spf13 <steve.francia@gmail.com>
2014-03-01 11:55:05 -0500

    bumping version for 0.10

--- commands/version.go
+++ commands/version.go
@@ -23,6 +23,6 @@ var version = &cobra.Command{
 	Short: "Print the version number of Hugo",
 	Long:  `All software has versions. This is Hugo's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9")
+		fmt.Println("Hugo Static Site Generator v0.10")
 	},
 }
