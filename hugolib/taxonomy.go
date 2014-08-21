4c735a78782e5b2e488729fa895fc757fcd0a6ed
Joel Scoble <joel.scoble@outlook.com>
2014-08-21 18:01:34 -0500

    preserve alias case while lowercasing taxonomy

--- hugolib/taxonomy.go
+++ hugolib/taxonomy.go
@@ -60,7 +60,7 @@ type OrderedTaxonomyEntry struct {
 
 // KeyPrep... Taxonomies should be case insensitive. Can make it easily conditional later.
 func kp(in string) string {
-	return helpers.MakePath(in)
+	return helpers.MakePathToLower(in)
 }
 
 func (i Taxonomy) Get(key string) WeightedPages { return i[kp(key)] }
