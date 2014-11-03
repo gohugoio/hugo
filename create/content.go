e127a5cb7d91aabc3df36e70123967a35df7aabf
Brian Payne <bpayne@uci.edu>
2014-11-02 17:29:55 -0800

    Added better error message for poorly formed frontmatters in archtypes

--- create/content.go
+++ create/content.go
@@ -57,6 +57,7 @@ func NewContent(kind, name string) (err error) {
 	}
 	newmetadata, err := cast.ToStringMapE(metadata)
 	if err != nil {
+		jww.ERROR.Println("Error processing archetype file:", location)
 		return err
 	}
 
