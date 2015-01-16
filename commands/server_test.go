508788e6949a51b76af15eb4570e4d73e7c7b09b
Anthony Fok <foka@debian.org>
2015-01-16 03:13:03 -0700

    Update TestFixUrl() to expect BaseUrl to have trailing `/`
    See #793, #799

--- commands/server_test.go
+++ commands/server_test.go
@@ -16,15 +16,15 @@ func TestFixUrl(t *testing.T) {
 		Result     string
 	}
 	tests := []data{
-		{"Basic http localhost", "", "http://foo.com", true, 1313, "http://localhost:1313"},
-		{"Basic https production, http localhost", "", "https://foo.com", true, 1313, "http://localhost:1313"},
-		{"Basic subdir", "", "http://foo.com/bar", true, 1313, "http://localhost:1313/bar"},
-		{"Basic production", "http://foo.com", "http://foo.com", false, 80, "http://foo.com"},
-		{"Production subdir", "http://foo.com/bar", "http://foo.com/bar", false, 80, "http://foo.com/bar"},
-		{"No http", "", "foo.com", true, 1313, "http://localhost:1313"},
-		{"Override configured port", "", "foo.com:2020", true, 1313, "http://localhost:1313"},
-		{"No http production", "foo.com", "foo.com", false, 80, "http://foo.com"},
-		{"No http production with port", "foo.com", "foo.com", true, 2020, "http://foo.com:2020"},
+		{"Basic http localhost", "", "http://foo.com", true, 1313, "http://localhost:1313/"},
+		{"Basic https production, http localhost", "", "https://foo.com", true, 1313, "http://localhost:1313/"},
+		{"Basic subdir", "", "http://foo.com/bar", true, 1313, "http://localhost:1313/bar/"},
+		{"Basic production", "http://foo.com", "http://foo.com", false, 80, "http://foo.com/"},
+		{"Production subdir", "http://foo.com/bar", "http://foo.com/bar", false, 80, "http://foo.com/bar/"},
+		{"No http", "", "foo.com", true, 1313, "http://localhost:1313/"},
+		{"Override configured port", "", "foo.com:2020", true, 1313, "http://localhost:1313/"},
+		{"No http production", "foo.com", "foo.com", false, 80, "http://foo.com/"},
+		{"No http production with port", "foo.com", "foo.com", true, 2020, "http://foo.com:2020/"},
 	}
 
 	for i, test := range tests {
