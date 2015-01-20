1b91fec0ac28eac8dde6a7431bab7b11f286bdce
bep <bjorn.erik.pedersen@gmail.com>
2015-01-20 17:15:12 +0100

    Add tests for IsSet

--- tpl/template_test.go
+++ tpl/template_test.go
@@ -361,6 +361,16 @@ func TestIntersect(t *testing.T) {
 	}
 }
 
+func TestIsSet(t *testing.T) {
+	aSlice := []interface{}{1, 2, 3, 5}
+	aMap := map[string]interface{}{"a": 1, "b": 2}
+
+	assert.True(t, IsSet(aSlice, 2))
+	assert.True(t, IsSet(aMap, "b"))
+	assert.False(t, IsSet(aSlice, 22))
+	assert.False(t, IsSet(aMap, "bc"))
+}
+
 func (x *TstX) TstRp() string {
 	return "r" + x.A
 }
