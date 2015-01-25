c02538a81d4ea3e6f65e279ef5f536201c444a23
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-01-25 20:15:20 -0330

    Add a test to demonstrate case-insensitivity.
    This test case demonstrates how the baseurl's case could be clobbered
    before the s.prepUrl() change.

--- hugolib/menu_test.go
+++ hugolib/menu_test.go
@@ -283,7 +283,7 @@ func doTestMenuWithUnicodeUrls(t *testing.T, canonifyUrls, uglyUrls bool) {
 	expectedBase := "/%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0"
 
 	if !canonifyUrls {
-		expectedBase = "/zoo" + expectedBase
+		expectedBase = "/Zoo" + expectedBase
 	}
 
 	var expected string
@@ -440,7 +440,7 @@ func getTestMenuState(s *Site, t *testing.T) *testMenuState {
 	}
 
 	viper.Set("menu", menus["menu"])
-	viper.Set("baseurl", "http://foo.local/zoo/")
+	viper.Set("baseurl", "http://foo.local/Zoo/")
 
 	return menuState
 }
