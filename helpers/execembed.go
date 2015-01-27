90f60a3ae4c7b7a543713d5acd4f489d8541d89f
afshar <afshar@google.com>
2015-01-27 13:25:41 -0800

    Now add the actual tempalte changes.

--- helpers/execembed.go
+++ helpers/execembed.go
@@ -10,15 +10,15 @@ import (
 func Exec(args ...string) string {
 	var out bytes.Buffer
 	var stderr bytes.Buffer
-  var arg []string
-  if len(args) == 0 {
-	  jww.ERROR.Print("Nothing to execute")
-    return "Nothing to execute"
-  }
+	var arg []string
+	if len(args) == 0 {
+		jww.ERROR.Print("Nothing to execute")
+		return "Nothing to execute"
+	}
 	name := args[0]
 	if len(args) > 1 {
-    arg = args[1 : len(args)-1]
-  }
+		arg = args[1 : len(args)-1]
+	}
 	cmd := exec.Command(name, arg...)
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
