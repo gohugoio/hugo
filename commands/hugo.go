be2842da18ebe3ec1a28a336ae4c48d889d302d1
bep <bjorn.erik.pedersen@gmail.com>
2015-05-21 20:05:14 +0200

    Fix formatting in hugo command description

--- commands/hugo.go
+++ commands/hugo.go
@@ -45,8 +45,7 @@ var HugoCmd = &cobra.Command{
 	Short: "hugo builds your site",
 	Long: `hugo is the main command, used to build your Hugo site. 
 	
-	Hugo is a Fast and Flexible Static Site Generator built with
-love by spf13 and friends in Go.
+Hugo is a Fast and Flexible Static Site Generator built with love by spf13 and friends in Go.
 
 Complete documentation is available at http://gohugo.io`,
 	Run: func(cmd *cobra.Command, args []string) {
