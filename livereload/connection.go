f045d7a611b5c609d44e894b3c1165ce24b67776
Steve Francia <steve.francia@gmail.com>
2015-11-23 22:16:36 -0500

    Change the license to Apache 2.0

--- livereload/connection.go
+++ livereload/connection.go
@@ -1,9 +1,9 @@
 // Copyright © 2014 Steve Francia <spf@spf13.com>.
 //
-// Licensed under the Simple Public License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// http://opensource.org/licenses/Simple-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
