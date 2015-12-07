e445c35d6a0c7f5fc2f90f31226cd1d46e048bbc
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2015-12-07 19:57:01 +0100

    Fix copyright headers in source files
    Still need to add some missing headers and an AUTHORS file.
    
    See #1646

--- commands/limit_darwin.go
+++ commands/limit_darwin.go
@@ -1,5 +1,5 @@
 // +build darwin
-// Copyright © 2013 Steve Francia <spf@spf13.com>.
+// Copyright 2015 The Hugo Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- source/filesystem.go
+++ source/filesystem.go
@@ -1,4 +1,4 @@
-// Copyright © 2014 Steve Francia <spf@spf13.com>.
+// Copyright 2015 The Hugo Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
