31035f8ad12f6bee967eb0a598e224e34acfd966
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2015-12-18 16:05:24 +0100

    Fix remote test on Go 1.4

--- tpl/template_resources_test.go
+++ tpl/template_resources_test.go
@@ -20,6 +20,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"os"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -115,7 +116,11 @@ func getTestServer(handler func(w http.ResponseWriter, r *http.Request)) (*httpt
 	testServer := httptest.NewServer(http.HandlerFunc(handler))
 	client := &http.Client{
 		Transport: &http.Transport{Proxy: func(r *http.Request) (*url.URL, error) {
-			r.Host = "Host: gohugo.io"
+
+			if !strings.Contains("go1.4", runtime.Version()) {
+				r.Host = "Host: gohugo.io"
+			}
+
 			return url.Parse(testServer.URL)
 		}},
 	}
