dd2ab2820ec9f894d56b7917a39dfdff5774075d
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2015-12-18 16:19:00 +0100

    Correct the strings.Contains syntax in failing test

--- tpl/template_resources_test.go
+++ tpl/template_resources_test.go
@@ -117,7 +117,7 @@ func getTestServer(handler func(w http.ResponseWriter, r *http.Request)) (*httpt
 	client := &http.Client{
 		Transport: &http.Transport{Proxy: func(r *http.Request) (*url.URL, error) {
 
-			if !strings.Contains("go1.4", runtime.Version()) {
+			if !strings.Contains(runtime.Version(), "go1.4") {
 				r.Host = "Host: gohugo.io"
 			}
 
