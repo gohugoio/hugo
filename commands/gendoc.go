bedc175952e96449d0837397652057bf8244a33e
Steve Francia <steve.francia@gmail.com>
2015-12-24 09:47:00 -0500

    Update to use cobra's sub package doc
    Backported to v0.15.docs to avoid build errors.

--- commands/gendoc.go
+++ commands/gendoc.go
@@ -2,14 +2,16 @@ package commands
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
-	"github.com/spf13/hugo/helpers"
-	"github.com/spf13/hugo/hugofs"
-	jww "github.com/spf13/jwalterweatherman"
 	"path"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+	"github.com/spf13/hugo/helpers"
+	"github.com/spf13/hugo/hugofs"
+	jww "github.com/spf13/jwalterweatherman"
 )
 
 const gendocFrontmatterTemplate = `---
@@ -53,7 +55,7 @@ for rendering in Hugo.`,
 		}
 
 		jww.FEEDBACK.Println("Generating Hugo command-line documentation in", gendocdir, "...")
-		cobra.GenMarkdownTreeCustom(cmd.Root(), gendocdir, prepender, linkHandler)
+		doc.GenMarkdownTreeCustom(cmd.Root(), gendocdir, prepender, linkHandler)
 		jww.FEEDBACK.Println("Done.")
 	},
 }
--- commands/genman.go
+++ commands/genman.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 	"github.com/spf13/hugo/helpers"
 	jww "github.com/spf13/jwalterweatherman"
 )
@@ -18,13 +19,13 @@ in the "man" directory under the current directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		genmandir := "man/"
 		cmd.Root().DisableAutoGenTag = true
-		header := &cobra.GenManHeader{
+		header := &doc.GenManHeader{
 			Section: "1",
 			Manual:  "Hugo Manual",
 			Source:  fmt.Sprintf("Hugo %s", helpers.HugoVersion()),
 		}
 		jww.FEEDBACK.Println("Generating Hugo man pages in", genmandir, "...")
-		cmd.Root().GenManTree(header, genmandir)
+		doc.GenManTree(cmd.Root(), header, genmandir)
 		jww.FEEDBACK.Println("Done.")
 	},
 }
