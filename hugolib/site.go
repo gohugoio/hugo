57eebbf243882cd0d036a19bdad80256ed3d54b1
Anthony Fok <foka@debian.org>
2016-01-30 17:39:17 +0800

    Use FEEDBACK instead of ERROR for empty home page warning
    jww.ERROR.Println() seems to have become too verbose,
    making the warning message intended for new users
    a lot less readable.

--- hugolib/site.go
+++ hugolib/site.go
@@ -1969,14 +1969,14 @@ func (s *Site) renderAndWritePage(name string, dest string, d interface{}, layou
 	if outBuffer.Len() == 0 {
 		jww.WARN.Printf("%q is rendered empty\n", dest)
 		if dest == "/" {
-			jww.ERROR.Println("=============================================================")
-			jww.ERROR.Println("Your rendered home page is blank: /index.html is zero-length")
-			jww.ERROR.Println(" * Did you specify a theme on the command-line or in your")
-			jww.ERROR.Printf("   %q file?  (Current theme: %q)\n", filepath.Base(viper.ConfigFileUsed()), viper.GetString("Theme"))
+			jww.FEEDBACK.Println("=============================================================")
+			jww.FEEDBACK.Println("Your rendered home page is blank: /index.html is zero-length")
+			jww.FEEDBACK.Println(" * Did you specify a theme on the command-line or in your")
+			jww.FEEDBACK.Printf("   %q file?  (Current theme: %q)\n", filepath.Base(viper.ConfigFileUsed()), viper.GetString("Theme"))
 			if !viper.GetBool("Verbose") {
-				jww.ERROR.Println(" * For more debugging information, run \"hugo -v\"")
+				jww.FEEDBACK.Println(" * For more debugging information, run \"hugo -v\"")
 			}
-			jww.ERROR.Println("=============================================================")
+			jww.FEEDBACK.Println("=============================================================")
 		}
 	}
 
