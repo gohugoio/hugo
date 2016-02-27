75cf324322fafe9f2b3f92de369752f413886353
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-02-27 23:21:07 +0100

    Set Date and Lastmod for main home page
    Fixes #1903

--- hugolib/site.go
+++ hugolib/site.go
@@ -1711,6 +1711,8 @@ func (s *Site) newHomeNode() *Node {
 	n.IsHome = true
 	s.setURLs(n, "/")
 	n.Data["Pages"] = s.Pages
+	n.Date = s.Pages[0].Date
+	n.Lastmod = s.Pages[0].Lastmod
 	return n
 }
 
