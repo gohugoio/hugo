6258d48b0220fa59edf50a32031677eb7b7944d4
srinivasreddy <thatiparthysreenivas@gmail.com>
2016-03-13 00:49:53 +0530

    Utils methods refactored for readability

--- utils/utils.go
+++ utils/utils.go
@@ -20,35 +20,37 @@ import (
 )
 
 func CheckErr(err error, s ...string) {
-	if err != nil {
-		if len(s) == 0 {
-			jww.CRITICAL.Println(err)
-		} else {
-			for _, message := range s {
-				jww.ERROR.Println(message)
-			}
-			jww.ERROR.Println(err)
-		}
+	if err == nil {
+		return
 	}
+	if len(s) == 0 {
+		jww.CRITICAL.Println(err)
+		return
+	}
+	for _, message := range s {
+		jww.ERROR.Println(message)
+	}
+	jww.ERROR.Println(err)
 }
 
 func StopOnErr(err error, s ...string) {
-	if err != nil {
-		if len(s) == 0 {
-			newMessage := err.Error()
+	if err == nil {
+		return
+	}
 
-			// Printing an empty string results in a error with
-			// no message, no bueno.
-			if newMessage != "" {
-				jww.CRITICAL.Println(newMessage)
-			}
-		} else {
-			for _, message := range s {
-				if message != "" {
-					jww.CRITICAL.Println(message)
-				}
-			}
+	defer os.Exit(-1)
+
+	if len(s) == 0 {
+		newMessage := err.Error()
+		// Printing an empty string results in a error with
+		// no message, no bueno.
+		if newMessage != "" {
+			jww.CRITICAL.Println(newMessage)
 		}
-		os.Exit(-1)
+	}
+	for _, message := range s {
+		if message != "" {
+			jww.CRITICAL.Println(message)
+		}
 	}
 }
