7d5c9fbf44ad9f2fd5baed405f79f2b132dd9178
Robert Basic <robertbasic.com@gmail.com>
2016-04-01 20:17:16 +0200

    Make ByCount sort consistently
    When two or more taxonomies have the same number of pages,
    sort them by name to have consistent ByCount sorting of
    taxonomies.
    
    Fixes #1930

--- hugolib/taxonomy_test.go
+++ hugolib/taxonomy_test.go
@@ -16,6 +16,8 @@ package hugolib
 import (
 	"strings"
 	"testing"
+
+	"github.com/spf13/viper"
 )
 
 func TestSitePossibleTaxonomies(t *testing.T) {
@@ -27,5 +29,31 @@ func TestSitePossibleTaxonomies(t *testing.T) {
 		if !compareStringSlice(taxonomies, []string{"categories", "tags"}) {
 			t.Fatalf("possible taxonomies do not match [tags categories].  Got: %s", taxonomies)
 		}
+	}
+}
+
+func TestByCountOrderOfTaxonomies(t *testing.T) {
+	viper.Reset()
+	defer viper.Reset()
+
+	taxonomies := make(map[string]string)
+
+	taxonomies["tag"] = "tags"
+	taxonomies["category"] = "categories"
+
+	viper.Set("taxonomies", taxonomies)
+
+	site := new(Site)
+	page, _ := NewPageFrom(strings.NewReader(pageYamlWithTaxonomiesA), "path/to/page")
+	site.Pages = append(site.Pages, page)
+	site.assembleTaxonomies()
+
+	st := make([]string, 0)
+	for _, t := range site.Taxonomies["tags"].ByCount() {
+		st = append(st, t.Name)
+	}
+
+	if !compareStringSlice(st, []string{"a", "b", "c"}) {
+		t.Fatalf("ordered taxonomies do not match [a, b, c].  Got: %s", st)
 	}
 }
