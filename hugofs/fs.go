dde967d8ca5c5856ed46b27ee6ba7948cde1b807
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-04-10 01:36:53 +0200

    hugofs: Add package doc

--- hugofs/fs.go
+++ hugofs/fs.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package hugofs provides the file systems used by Hugo.
 package hugofs
 
 import (
