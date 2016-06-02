bef689a33ab431d093451bf58410b9560e19d3a4
Steve Francia <steve.francia@gmail.com>
2016-06-02 15:25:19 -0400

    Annotate temp files that Hugo ignores

--- commands/hugo.go
+++ commands/hugo.go
@@ -742,15 +742,15 @@ func NewWatcher(port int) error {
 				for _, ev := range evs {
 					ext := filepath.Ext(ev.Name)
 					istemp := strings.HasSuffix(ext, "~") ||
-						(ext == ".swp") ||
-						(ext == ".swx") ||
-						(ext == ".tmp") ||
-						(ext == ".DS_Store") ||
-						filepath.Base(ev.Name) == "4913" ||
-						strings.HasPrefix(ext, ".goutputstream") ||
-						strings.HasSuffix(ext, "jb_old___") ||
-						strings.HasSuffix(ext, "jb_bak___") ||
-						strings.HasPrefix(ext, ".sb-")
+						(ext == ".swp") || // vim
+						(ext == ".swx") || // vim
+						(ext == ".tmp") || // generic temp file
+						(ext == ".DS_Store") || // OSX Thumbnail
+						filepath.Base(ev.Name) == "4913" || // vim
+						strings.HasPrefix(ext, ".goutputstream") || // gnome
+						strings.HasSuffix(ext, "jb_old___") || // intelliJ
+						strings.HasSuffix(ext, "jb_bak___") || // intelliJ
+						strings.HasPrefix(ext, ".sb-") // byword
 					if istemp {
 						continue
 					}
