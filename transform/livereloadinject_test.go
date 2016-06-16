7e08d23eebe8de8c688150900423dca6234b22a8
Dragos Plesca <dragos.plesca@gmail.com>
2016-06-16 19:43:47 +0300

    transform: Explicitly bind LiveReload to server port
    If hugo server is run on port 80 or 443, LiveReload does not
    correctly bind to the same port, instead using port 35729.
    This commit adds functionality to inform LiveReload of the
    correct port to bind to.
    
    See https://github.com/livereload/livereload-js/issues/16
    
    Partially contributed by Jeff Minard (@chuyskywalker).
    
    Fixes #2205

--- transform/livereloadinject_test.go
+++ transform/livereloadinject_test.go
@@ -18,6 +18,8 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/spf13/viper"
 )
 
 func TestLiveReloadInject(t *testing.T) {
@@ -26,13 +28,14 @@ func TestLiveReloadInject(t *testing.T) {
 }
 
 func doTestLiveReloadInject(t *testing.T, bodyEndTag string) {
+	viper.Set("port", 1313)
 	out := new(bytes.Buffer)
 	in := strings.NewReader(bodyEndTag)
 
 	tr := NewChain(LiveReloadInject)
 	tr.Apply(out, in, []byte("path"))
 
-	expected := fmt.Sprintf(`<script data-no-instant>document.write('<script src="/livereload.js?mindelay=10"></' + 'script>')</script>%s`, bodyEndTag)
+	expected := fmt.Sprintf(`<script data-no-instant>document.write('<script src="/livereload.js?port=1313&mindelay=10"></' + 'script>')</script>%s`, bodyEndTag)
 	if string(out.Bytes()) != expected {
 		t.Errorf("Expected %s got %s", expected, string(out.Bytes()))
 	}
