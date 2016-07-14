ef9b915680e21d5ab5fceb7c7957fd0b626942a7
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-07-14 11:29:21 +0200

    Revert "Throw a noisy error when a post has no layout"
    We have to figure out another way. There are perfectly valid reasons not having a layout for a page (I have have some broken sites as result of this).
    
    See #1313
    
    This reverts commit b15934008fd6a1e7bcf9b8a47ab66e33271c4dca.

--- hugolib/site.go
+++ hugolib/site.go
@@ -2025,7 +2025,7 @@ func (s *Site) renderAndWritePage(name string, dest string, d interface{}, layou
 func (s *Site) render(name string, d interface{}, w io.Writer, layouts ...string) error {
 	layout, found := s.findFirstLayout(layouts...)
 	if found == false {
-		jww.ERROR.Printf("Unable to locate layout for %s: %s\n", name, layouts)
+		jww.WARN.Printf("Unable to locate layout for %s: %s\n", name, layouts)
 		return nil
 	}
 
