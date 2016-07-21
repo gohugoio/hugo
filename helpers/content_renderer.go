a7565fdea44bacdc6219e5324dead86b3150c0d6
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-07-21 21:30:05 +0200

    Add CSS class to TODO list and list items
    See #2296

--- helpers/content_renderer.go
+++ helpers/content_renderer.go
@@ -72,16 +72,33 @@ func (renderer *HugoHTMLRenderer) Image(out *bytes.Buffer, link []byte, title []
 	}
 }
 
-// Task List support.
+// ListItem adds task list support to the Blackfriday renderer.
 func (renderer *HugoHTMLRenderer) ListItem(out *bytes.Buffer, text []byte, flags int) {
 	switch {
 	case bytes.HasPrefix(text, []byte("[ ] ")):
-		text = append([]byte(`<input type="checkbox" disabled="">`), text[3:]...)
+		text = append([]byte(`<input type="checkbox" disabled="" class="task-list-item">`), text[3:]...)
+
 	case bytes.HasPrefix(text, []byte("[x] ")) || bytes.HasPrefix(text, []byte("[X] ")):
-		text = append([]byte(`<input type="checkbox" checked="" disabled="">`), text[3:]...)
+		text = append([]byte(`<input type="checkbox" checked="" disabled="" class="task-list-item">`), text[3:]...)
 	}
 
 	renderer.Renderer.ListItem(out, text, flags)
+}
+
+// List adds task list support to the Blackfriday renderer.
+func (renderer *HugoHTMLRenderer) List(out *bytes.Buffer, text func() bool, flags int) {
+	marker := out.Len()
+	renderer.Renderer.List(out, text, flags)
+	if out.Len() > marker {
+		list := out.Bytes()[marker:]
+		if bytes.Contains(list, []byte("task-list-item")) {
+			// Rewrite the buffer from the marker
+			out.Truncate(marker)
+			// May be either dl, ul or ol
+			list := append(list[:4], append([]byte(` class="task-list"`), list[4:]...)...)
+			out.Write(list)
+		}
+	}
 }
 
 // HugoMmarkHTMLRenderer wraps a mmark.Renderer, typically a mmark.html
