6b4d3263b67d7145bb6482ae8f551f0b28ab8068
Anthony Fok <foka@debian.org>
2016-09-26 14:23:57 -0600

    commands: Revise instructions after "hugo new site"
    mainly to fit inside 80-column wide terminal windows.
    
    See #1164

--- commands/new.go
+++ commands/new.go
@@ -153,12 +153,14 @@ func doNewSite(basepath string, force bool) error {
 	jww.FEEDBACK.Printf("Congratulations! Your new Hugo site is created in %q.\n\n", basepath)
 	jww.FEEDBACK.Println(`Just a few more steps and you're ready to go:
 
-1. Download a theme into the same-named folder. Choose a theme from https://themes.gohugo.io or
-   create your own with the "hugo new theme <THEMENAME>" command
-2. Perhaps you want to add some content. You can add single files with "hugo new <SECTIONNAME>/<FILENAME>.<FORMAT>"
-3. Start the built-in live server via "hugo server"
+1. Download a theme into the same-named folder.
+   Choose a theme from https://themes.gohugo.io/, or
+   create your own with the "hugo new theme <THEMENAME>" command.
+2. Perhaps you want to add some content. You can add single files
+   with "hugo new <SECTIONNAME>/<FILENAME>.<FORMAT>".
+3. Start the built-in live server via "hugo server".
 
-For more information read the documentation at https://gohugo.io.`)
+Visit https://gohugo.io/ for quickstart guide and full documentation.`)
 
 	return nil
 }
