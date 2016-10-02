18d899c581b63b63b416658b2e6895fadbdbb391
Mark D. Blackwell <markdblackwell01@gmail.com>
2016-10-01 22:19:22 -0400

    docs: Improve home page
    Organize CSS and Javascript files into directories, separating vendor
    source code from home-grown.
    
    Separate new properties and selectors in Bootstrap CSS (already edited
    for Hugo) from:
    1. Original property values; and
    2. Changed property values.
    
    For the home page:
    
    1. Clarify the English text;
    2. Add some element names to classes in CSS selectors (to speed loading);
    3. Document the Owl Carousel and Highlight.js versions;
    4. Add Owl Carousel's license;
    5. Restore HTML comments to browser source views;
    6. Make the beginning GitHub button transparent on focus (like the others);
    
    7. For the final, colored Twitter and Github buttons:
      a. Remove the dotted box on focus; and
      b. Color them green on active.
    
    8. To increase (layout/) source readability:
      a. Place `class` attributes before `href` attributes;
      b. In `class` attributes, place more-specific class names before
         less-specific; and
      c. Comment the beginnings and ends of Bootstrap rows.
    
    9. To increase understandability during development:
      a. Document the meaning of CSS file, `HPstyles` by renaming it to
         `home-page-style`;
      b. Move inline styling to CSS stylesheets;
      c. Separate @media queries to a file;
      d. Separate the inline Owl Carousel custom Javascript to a file;
      e. Use regular Owl Carousel CSS files instead of minified ones; and
      f. Rename the wide, landscape desk background image filename from
         `desk-long` to `desk-wide`.

---
+++ static/js/owl.carousel-custom.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:3
+        },
+    }
+})
