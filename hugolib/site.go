c87331ae8427b578655a88e8fb6286aa4774d060
Chase Adams <realchaseadams@gmail.com>
2016-11-29 15:03:47 -0800

    hugolib: Fix rendered empty msg
    Changes `%q` to `%s`. `%q` was safely escaping the `\` in windows so that it was printing `\\`.

--- hugolib/site.go
+++ hugolib/site.go
@@ -1768,7 +1768,7 @@ func (s *Site) renderAndWritePage(name string, dest string, d interface{}, layou
 
 	if outBuffer.Len() == 0 {
 
-		jww.WARN.Printf("%q is rendered empty\n", dest)
+		jww.WARN.Printf("%s is rendered empty\n", dest)
 		if dest == "/" {
 			debugAddend := ""
 			if !viper.GetBool("verbose") {
