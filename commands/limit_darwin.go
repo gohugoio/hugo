c60c41e051698292929f5c1f34f1760a6a37b560
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-01-05 15:43:48 +0100

    commans: Remove unneeded build constraint
    Fixes #2886

--- commands/limit_darwin.go
+++ commands/limit_darwin.go
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin
 // Copyright 2015 The Hugo Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
