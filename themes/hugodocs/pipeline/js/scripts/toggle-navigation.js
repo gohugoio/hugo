212241b8c5ce206969107b5b283f7d37b2bb5ada
Ryan Watters <watters.ryan@gmail.com>
2017-02-11 16:55:31 -0800

    Fix footer on mobile when menu is open

--- themes/hugodocs/pipeline/js/scripts/toggle-navigation.js
+++ themes/hugodocs/pipeline/js/scripts/toggle-navigation.js
@@ -2,11 +2,11 @@
 $('#navigation-toggle').on('click', function(evt) {
 	evt.preventDefault();
 	evt.stopPropagation();
-  $('#site-navigation,.all-content-wrapper,#navigation-toggle').toggleClass('navigation-open');
+  $('#site-navigation,.all-content-wrapper,#navigation-toggle,#site-footer').toggleClass('navigation-open');
 });
 //close navigation if body content is clicked when docs are open
 $('#all-content-wrapper').on('click', function() {
   if ($('.site-navigation.navigation-open')) {
-    $('.site-navigation.navigation-open,.all-content-wrapper.navigation-open,#navigation-toggle').removeClass('navigation-open');
+    $('.site-navigation.navigation-open,.all-content-wrapper.navigation-open,#navigation-toggle,#site-footer').removeClass('navigation-open');
   }
 });
