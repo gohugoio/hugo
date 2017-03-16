d76e5f36b412eb68be3cd5a53bacc099ec46280f
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-03-16 09:09:26 +0100

    hugolib: Pull all alias handling into one file

--- hugolib/site_writer.go
+++ hugolib/site_writer.go
@@ -14,11 +14,8 @@
 package hugolib
 
 import (
-	"fmt"
 	"io"
 	"path/filepath"
-	"runtime"
-	"strings"
 
 	"github.com/spf13/hugo/helpers"
 	"github.com/spf13/hugo/hugofs"
@@ -85,75 +82,6 @@ func (w siteWriter) baseTargetPathPage(f output.Format, src string) (string, err
 
 func (w siteWriter) targetPathFile(src string) (string, error) {
 	return filepath.Join(w.publishDir, filepath.FromSlash(src)), nil
-}
-
-func (w siteWriter) targetPathAlias(src string) (string, error) {
-	originalAlias := src
-	if len(src) <= 0 {
-		return "", fmt.Errorf("Alias \"\" is an empty string")
-	}
-
-	alias := filepath.Clean(src)
-	components := strings.Split(alias, helpers.FilePathSeparator)
-
-	if !w.allowRoot && alias == helpers.FilePathSeparator {
-		return "", fmt.Errorf("Alias \"%s\" resolves to website root directory", originalAlias)
-	}
-
-	// Validate against directory traversal
-	if components[0] == ".." {
-		return "", fmt.Errorf("Alias \"%s\" traverses outside the website root directory", originalAlias)
-	}
-
-	// Handle Windows file and directory naming restrictions
-	// See "Naming Files, Paths, and Namespaces" on MSDN
-	// https://msdn.microsoft.com/en-us/library/aa365247%28v=VS.85%29.aspx?f=255&MSPPError=-2147217396
-	msgs := []string{}
-	reservedNames := []string{"CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"}
-
-	if strings.ContainsAny(alias, ":*?\"<>|") {
-		msgs = append(msgs, fmt.Sprintf("Alias \"%s\" contains invalid characters on Windows: : * ? \" < > |", originalAlias))
-	}
-	for _, ch := range alias {
-		if ch < ' ' {
-			msgs = append(msgs, fmt.Sprintf("Alias \"%s\" contains ASCII control code (0x00 to 0x1F), invalid on Windows: : * ? \" < > |", originalAlias))
-			continue
-		}
-	}
-	for _, comp := range components {
-		if strings.HasSuffix(comp, " ") || strings.HasSuffix(comp, ".") {
-			msgs = append(msgs, fmt.Sprintf("Alias \"%s\" contains component with a trailing space or period, problematic on Windows", originalAlias))
-		}
-		for _, r := range reservedNames {
-			if comp == r {
-				msgs = append(msgs, fmt.Sprintf("Alias \"%s\" contains component with reserved name \"%s\" on Windows", originalAlias, r))
-			}
-		}
-	}
-	if len(msgs) > 0 {
-		if runtime.GOOS == "windows" {
-			for _, m := range msgs {
-				w.log.ERROR.Println(m)
-			}
-			return "", fmt.Errorf("Cannot create \"%s\": Windows filename restriction", originalAlias)
-		}
-		for _, m := range msgs {
-			w.log.WARN.Println(m)
-		}
-	}
-
-	// Add the final touch
-	alias = strings.TrimPrefix(alias, helpers.FilePathSeparator)
-	if strings.HasSuffix(alias, helpers.FilePathSeparator) {
-		alias = alias + "index.html"
-	} else if !strings.HasSuffix(alias, ".html") {
-		alias = alias + helpers.FilePathSeparator + "index.html"
-	}
-	if originalAlias != alias {
-		w.log.INFO.Printf("Alias \"%s\" translated to \"%s\"\n", originalAlias, alias)
-	}
-
-	return filepath.Join(w.publishDir, alias), nil
 }
 
 func extension(ext string) string {
