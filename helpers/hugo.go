0f78dd06a42f2d85e95cf69348ce2d13a0398627
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-06-13 14:22:14 +0200

    releaser: Prepare repository for 0.22.1-DEV
    [ci skip]

--- helpers/hugo.go
+++ helpers/hugo.go
@@ -64,7 +64,7 @@ func (v HugoVersion) NextPatchLevel(level int) HugoVersion {
 var CurrentHugoVersion = HugoVersion{
 	Number:     0.22,
 	PatchLevel: 1,
-	Suffix:     "",
+	Suffix:     "-DEV",
 }
 
 func hugoVersion(version float32, patchVersion int, suffix string) string {
