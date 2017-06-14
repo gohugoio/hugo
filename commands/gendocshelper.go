729be8074bddb58c9111f32c55cc769e49cd0d5a
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-06-14 10:37:58 +0200

    commands: Adjust docs path

--- commands/gendocshelper.go
+++ commands/gendocshelper.go
@@ -41,7 +41,9 @@ func createGenDocsHelper() *genDocsHelper {
 		return g.generate()
 	}
 
-	g.cmd.PersistentFlags().StringVarP(&g.target, "dir", "", "docs/data", "data dir")
+	// Note that ./docs is a submodule, and writing to that would be suboptimal.
+	// Let us assume that the default is a sibling project.
+	g.cmd.PersistentFlags().StringVarP(&g.target, "dir", "", "../hugoDocs/data", "data dir")
 
 	return g
 }
