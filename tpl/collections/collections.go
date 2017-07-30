140c755754e0fcd28d602281bffa025a07476619
Daniel Karlsson <danielk@fliffr.com>
2017-07-30 12:51:27 +0200

    calling isSet on array with key type string no longer panics but returns false
    Fixes #3681

--- tpl/collections/collections.go
+++ tpl/collections/collections.go
@@ -313,6 +313,10 @@ func (ns *Namespace) IsSet(a interface{}, key interface{}) (bool, error) {
 
 	switch av.Kind() {
 	case reflect.Array, reflect.Chan, reflect.Slice:
+		if kv.Kind() != reflect.Int && kv.Kind() != reflect.Int8 && kv.Kind() != reflect.Int16 && kv.Kind() != reflect.Int32 && kv.Kind() != reflect.Int64 {
+			helpers.DistinctFeedbackLog.Printf("WARNING: calling IsSet with with key of type %q will always return false for container of type %q (%T).\n", kv.Kind(), av.Kind(), a)
+			return false, nil
+		}
 		if int64(av.Len()) > kv.Int() {
 			return true, nil
 		}
