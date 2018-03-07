9ac62065840aa27c6e35ed3348fb76aa1288eff2
hugoreleaser <bjorn.erik.pedersen+hugoreleaser@gmail.com>
2018-03-07 17:58:48 +0000

    releaser: Prepare repository for 0.38-DEV
    [ci skip]

--- helpers/hugo.go
+++ helpers/hugo.go
@@ -123,9 +123,9 @@ func (v HugoVersion) NextPatchLevel(level int) HugoVersion {
 // CurrentHugoVersion represents the current build version.
 // This should be the only one.
 var CurrentHugoVersion = HugoVersion{
-	Number:     0.37,
-	PatchLevel: 1,
-	Suffix:     "",
+	Number:     0.38,
+	PatchLevel: 0,
+	Suffix:     "-DEV",
 }
 
 func hugoVersion(version float32, patchVersion int, suffix string) string {
