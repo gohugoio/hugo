2817e842407c8dcbfc738297ab634392fcb41ce1
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-04-07 10:10:06 +0200

    Fix handling of content files with "." in them
    As in, more dots than just to separate the extension and any language indicator.
    
    Fixes #4559

--- hugofs/language_fs_test.go
+++ hugofs/language_fs_test.go
@@ -52,3 +52,47 @@ func TestLanguagFs(t *testing.T) {
 	assert.Equal("page.md", lfi.RealName())
 
 }
+
+// Issue 4559
+func TestFilenamesHandling(t *testing.T) {
+	languages := map[string]bool{
+		"sv": true,
+	}
+	base := filepath.FromSlash("/my/base")
+	assert := require.New(t)
+	m := afero.NewMemMapFs()
+	bfs := afero.NewBasePathFs(m, base)
+	lfs := NewLanguageFs("sv", languages, bfs)
+	assert.NotNil(lfs)
+	assert.Equal("sv", lfs.Lang())
+
+	for _, test := range []struct {
+		filename string
+		check    func(fi *LanguageFileInfo)
+	}{
+		{"tc-lib-color/class-Com.Tecnick.Color.Css", func(fi *LanguageFileInfo) {
+			assert.Equal("class-Com.Tecnick.Color", fi.TranslationBaseName())
+			assert.Equal(filepath.FromSlash("/my/base"), fi.BaseDir())
+			assert.Equal(filepath.FromSlash("tc-lib-color/class-Com.Tecnick.Color.Css"), fi.Path())
+			assert.Equal("class-Com.Tecnick.Color.Css", fi.RealName())
+			assert.Equal(filepath.FromSlash("/my/base/tc-lib-color/class-Com.Tecnick.Color.Css"), fi.Filename())
+		}},
+		{"tc-lib-color/class-Com.Tecnick.Color.sv.Css", func(fi *LanguageFileInfo) {
+			assert.Equal("class-Com.Tecnick.Color", fi.TranslationBaseName())
+			assert.Equal("class-Com.Tecnick.Color.sv.Css", fi.RealName())
+			assert.Equal(filepath.FromSlash("/my/base/tc-lib-color/class-Com.Tecnick.Color.sv.Css"), fi.Filename())
+		}},
+	} {
+		err := afero.WriteFile(lfs, filepath.FromSlash(test.filename), []byte("abc"), 0777)
+		assert.NoError(err)
+		fi, err := lfs.Stat(filepath.FromSlash(test.filename))
+		assert.NoError(err)
+
+		lfi, ok := fi.(*LanguageFileInfo)
+		assert.True(ok)
+		assert.Equal("sv", lfi.Lang())
+		test.check(lfi)
+
+	}
+
+}
