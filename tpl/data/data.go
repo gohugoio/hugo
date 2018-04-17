c3058a9a36eea21d4688eb237100227fbf9ac946
Lucas Liberacki <coolcodingguy@gmail.com>
2018-04-17 00:17:46 -0400

    Updated GetCSV error message

--- tpl/data/data.go
+++ tpl/data/data.go
@@ -59,7 +59,7 @@ func (ns *Namespace) GetCSV(sep string, urlParts ...string) (d [][]string, err e
 		var req *http.Request
 		req, err = http.NewRequest("GET", url, nil)
 		if err != nil {
-			jww.ERROR.Printf("Failed to create request for getJSON: %s", err)
+			jww.ERROR.Printf("Failed to create request for getCSV: %s", err)
 			return nil, err
 		}
 
