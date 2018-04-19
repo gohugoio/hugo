e590cc26eb1363a4b84603f051b20bd43fd1f7bd
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-04-19 12:04:34 +0200

    Improve .Content vs shortcodes
    For the content from other pages in shortcodes there are some chicken and
    egg dependencies that is hard to get around. But we can improve on this  by preparing the pages in a certain order:
    
     1. The headless bundles goes first. These are page typically page and image collections..
     2. Leaf bundles
     3. Regular single pages
     4. Branch bundles
    
    Fixes #4632

--- hugolib/hugo_sites_build.go
+++ hugolib/hugo_sites_build.go
@@ -224,6 +224,7 @@ func (h *HugoSites) render(config *BuildCfg) error {
 		s.initRenderFormats()
 		for i, rf := range s.renderFormats {
 			s.rc = &siteRenderingContext{Format: rf}
+
 			s.preparePagesForRender(config)
 
 			if !config.SkipRender {
