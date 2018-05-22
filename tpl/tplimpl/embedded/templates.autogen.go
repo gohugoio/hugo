14705ecead09a0ad8dfa4de2d9b5ba9188c32d07
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-05-22 09:11:34 +0200

    tpl: Add another class and an id to youtube_simple
    To provide some more styling options.
    
    See #4616

--- tpl/tplimpl/embedded/templates.autogen.go
+++ tpl/tplimpl/embedded/templates.autogen.go
@@ -341,7 +341,9 @@ M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.5
 {{/* If class is set, assume the user wants to provide his own styles. */}}
 {{ template "__h_simple_css" $ }}
 {{ end }}
-<div class="{{ $class }}">
+{{ $secondClass := "s_youtube_simple" }}
+{{ $divID := printf "%s_%d" $secondClass (add $.Ordinal 1) }}
+<div class="{{ $secondClass }} {{ $class }}" id="{{ $divID }}">
 {{ $tb := printf "//i.ytimg.com/vi/%s/" $id }}
 <a href="//youtube.com/watch?v={{ $id | safeHTMLAttr }}" target="_blank">
      <img src="{{ printf "%smaxresdefault.jpg" $tb }}" srcset="{{ printf "%shqdefault.jpg" $tb }} 1x {{ printf "%smaxresdefault.jpg" $tb }} 2x" alt="Video">
