a7acd073ca9432d1275de05a521e5641c880461b
Anthony Fok <foka@debian.org>
2018-06-11 01:38:50 -0600

    commands: Move nextStepsText() to new_site.go

--- commands/new.go
+++ commands/new.go
@@ -126,27 +126,6 @@ func (n *newCmd) newContent(cmd *cobra.Command, args []string) error {
 	return create.NewContent(ps, siteFactory, kind, createPath)
 }
 
-func nextStepsText() string {
-	var nextStepsText bytes.Buffer
-
-	nextStepsText.WriteString(`Just a few more steps and you're ready to go:
-
-1. Download a theme into the same-named folder.
-   Choose a theme from https://themes.gohugo.io/, or
-   create your own with the "hugo new theme <THEMENAME>" command.
-2. Perhaps you want to add some content. You can add single files
-   with "hugo new `)
-
-	nextStepsText.WriteString(filepath.Join("<SECTIONNAME>", "<FILENAME>.<FORMAT>"))
-
-	nextStepsText.WriteString(`".
-3. Start the built-in live server via "hugo server".
-
-Visit https://gohugo.io/ for quickstart guide and full documentation.`)
-
-	return nextStepsText.String()
-}
-
 func mkdir(x ...string) {
 	p := filepath.Join(x...)
 
