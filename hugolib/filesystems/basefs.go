ea2df530c184c61c77143ca5d65a96bffca01156
Cameron Moore <moorereason@gmail.com>
2018-08-28 15:44:42 -0500

    hugolib: Fix godoc comment

--- hugolib/filesystems/basefs.go
+++ hugolib/filesystems/basefs.go
@@ -183,7 +183,7 @@ func (s SourceFilesystems) IsData(filename string) bool {
 	return s.Data.Contains(filename)
 }
 
-// IsAsset returns true if the given filename is a member of the data filesystem.
+// IsAsset returns true if the given filename is a member of the asset filesystem.
 func (s SourceFilesystems) IsAsset(filename string) bool {
 	return s.Assets.Contains(filename)
 }
