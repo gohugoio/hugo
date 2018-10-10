8ba9a273669e98a0e8d2f7db0291acc3a0a66ee1
hugoreleaser <bjorn.erik.pedersen+hugoreleaser@gmail.com>
2018-10-10 10:36:37 +0000

    releaser: Bump versions for release of 0.49.1
    [ci skip]

--- helpers/hugo.go
+++ helpers/hugo.go
@@ -126,7 +126,7 @@ func (v HugoVersion) NextPatchLevel(level int) HugoVersion {
 // This should be the only one.
 var CurrentHugoVersion = HugoVersion{
 	Number:     0.49,
-	PatchLevel: 0,
+	PatchLevel: 1,
 	Suffix:     "",
 }
 
