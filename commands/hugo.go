6b78b3810a800e315b57e2d907db9040cda36ac0
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-10-24 22:15:57 +0200

    Revert "commands: Read disableFastRender from flag even if it's not changed"
    On second thought, removing this isn't worth it.
    
    This reverts commit 78a4c2e32ef9ea8e92bb7bb3586e4c22b02eb494.

--- commands/hugo.go
+++ commands/hugo.go
@@ -219,7 +219,6 @@ func initializeFlags(cmd *cobra.Command, cfg config.Provider) {
 
 	// Will set a value even if it is the default.
 	flagKeysForced := []string{
-		"disableFastRender",
 		"minify",
 	}
 
