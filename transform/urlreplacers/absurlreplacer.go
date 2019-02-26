c154c2f7b2a6703dbde7f6bd2a1817a39c6fd2ea
Berin Larson <larson004@gmail.com>
2019-02-26 13:41:06 +0530

    Rewrite relative action URLS
    Fixes #5701

--- transform/urlreplacers/absurlreplacer.go
+++ transform/urlreplacers/absurlreplacer.go
@@ -49,6 +49,7 @@ func newPrefixState() []*prefix {
 	return []*prefix{
 		{b: []byte("src="), f: checkCandidateBase},
 		{b: []byte("href="), f: checkCandidateBase},
+		{b: []byte("action="), f: checkCandidateBase},
 		{b: []byte("srcset="), f: checkCandidateSrcset},
 	}
 }
