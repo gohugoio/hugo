8559f5c29f20b7b5188f93f8b1d9e510e3dee4f5
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-04-05 08:55:39 +0200

    Use YAML for the benchmark compare

--- benchbep.sh
+++ benchbep.sh
@@ -1,2 +1,2 @@
-gobench -package=./hugolib -bench="BenchmarkSiteBuilding/TOML,num_langs=3,num_pages=5000,tags_per_page=5,shortcodes,render" -count=3 > 1.bench
+gobench -package=./hugolib -bench="BenchmarkSiteBuilding/YAML,num_langs=3,num_pages=5000,tags_per_page=5,shortcodes,render" -count=3 > 1.bench
 benchcmp -best 0.bench 1.bench
