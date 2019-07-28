b294048e9b71c7b4c9b91ee1f91f09a4627efcf1
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-07-28 12:43:54 +0200

    Make it a Hugo Module

--- themes/gohugoioTheme/archetypes/showcase.md
+++
@@ -1,7 +0,0 @@
----
-title: "{{ replace .Name "-" " " | title }}"
-date: {{ .Date }}
-description:
-siteURL:
-siteSource:
----
--- themes/gohugoioTheme/assets/js/lazysizes.js
+++
@@ -1,3 +0,0 @@
-var lazysizes = require('lazysizes');
-// var lsnoscript = require('lazysizes/plugins/noscript/ls.noscript.js');
-var unveilhooks = require('lazysizes/plugins/unveilhooks/ls.unveilhooks.js');
