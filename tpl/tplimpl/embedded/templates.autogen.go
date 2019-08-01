2d1d33673d82c5073335e18944744606a71a5029
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-08-01 14:05:20 +0200

    tpl: Regenerate templates

--- tpl/tplimpl/embedded/templates.autogen.go
+++ tpl/tplimpl/embedded/templates.autogen.go
@@ -268,7 +268,8 @@ if (!doNotTrack) {
     </li>
     {{ end }}
 </ul>
-{{ end }}`},
+{{ end }}
+`},
 	{`schema.html`, `<meta itemprop="name" content="{{ .Title }}">
 <meta itemprop="description" content="{{ with .Description }}{{ . }}{{ else }}{{if .IsPage}}{{ .Summary }}{{ else }}{{ with .Site.Params.description }}{{ . }}{{ end }}{{ end }}{{ end }}">
 
@@ -347,7 +348,8 @@ if (!doNotTrack) {
     {{- end }}
 </figure>
 `},
-	{`shortcodes/gist.html`, `<script type="application/javascript" src="//gist.github.com/{{ index .Params 0 }}/{{ index .Params 1 }}.js{{if len .Params | eq 3 }}?file={{ index .Params 2 }}{{end}}"></script>`},
+	{`shortcodes/gist.html`, `<script type="application/javascript" src="https://gist.github.com/{{ index .Params 0 }}/{{ index .Params 1 }}.js{{if len .Params | eq 3 }}?file={{ index .Params 2 }}{{end}}"></script>
+`},
 	{`shortcodes/highlight.html`, `{{ if len .Params | eq 2 }}{{ highlight (trim .Inner "\n\r") (.Get 0) (.Get 1) }}{{ else }}{{ highlight (trim .Inner "\n\r") (.Get 0) "" }}{{ end }}`},
 	{`shortcodes/instagram.html`, `{{- $pc := .Page.Site.Config.Privacy.Instagram -}}
 {{- if not $pc.Disable -}}
