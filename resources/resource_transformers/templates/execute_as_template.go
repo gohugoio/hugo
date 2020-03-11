18cb21ff2e4a60e7094908e4d6113a9d5a086316
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2020-03-11 22:21:16 +0100

    resources: Add data context to the key in ExecuteAsTemplate
    Fixes #7046

--- resources/resource_transformers/templates/execute_as_template.go
+++ resources/resource_transformers/templates/execute_as_template.go
@@ -48,7 +48,7 @@ type executeAsTemplateTransform struct {
 }
 
 func (t *executeAsTemplateTransform) Key() internal.ResourceTransformationKey {
-	return internal.NewResourceTransformationKey("execute-as-template", t.targetPath)
+	return internal.NewResourceTransformationKey("execute-as-template", t.targetPath, t.data)
 }
 
 func (t *executeAsTemplateTransform) Transform(ctx *resources.ResourceTransformationCtx) error {
