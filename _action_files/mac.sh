2d12d9e3bfbead949278173e70b32e4d6e86c659
hamelsmu <hamel.husain@gmail.com>
2020-10-25 21:20:04 -0700

    test artifact upload

--- _action_files/mac.sh
+++ _action_files/mac.sh
@@ -1,3 +1,6 @@
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+brew install upx
 wget https://golang.org/dl/go1.15.3.darwin-amd64.pkg
 sudo installer -pkg go1.15.3.darwin-amd64.pkg -target /
 go version
+go install --tags extended
