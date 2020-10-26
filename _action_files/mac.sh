94aae1180ff45844bf2395cfdc20ffe4b00f9a4c
hamelsmu <hamel.husain@gmail.com>
2020-10-25 21:21:47 -0700

    remove brew install

--- _action_files/mac.sh
+++ _action_files/mac.sh
@@ -1,4 +1,3 @@
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
 brew install upx
 wget https://golang.org/dl/go1.15.3.darwin-amd64.pkg
 sudo installer -pkg go1.15.3.darwin-amd64.pkg -target /
