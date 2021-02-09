7118f89cf35246767e26dcb5e747469ffa61f473
Anthony Fok <foka@debian.org>
2021-02-09 09:57:18 -0700

    Refer to mage instead of make in comment regarding commitHash

--- common/hugo/hugo.go
+++ common/hugo/hugo.go
@@ -34,8 +34,8 @@ const (
 )
 
 var (
-	// commitHash contains the current Git revision. Use make to build to make
-	// sure this gets set.
+	// commitHash contains the current Git revision.
+	// Use mage to build to make sure this gets set.
 	commitHash string
 
 	// buildDate contains the date of the current build.
