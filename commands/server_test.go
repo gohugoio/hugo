10307da643d2acc65d3b50b174339ba680406677
satotake <doublequotation@gmail.com>
2021-05-17 03:01:37 +0900

    add TestServerRenderStaticToDisk

--- commands/server_test.go
+++ commands/server_test.go
@@ -15,8 +15,10 @@ package commands
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
@@ -39,7 +41,7 @@ func TestServer(t *testing.T) {
 	c.Assert(err, qt.IsNil)
 
 	// Let us hope that this port is available on all systems ...
-	port := 1331
+	port := 1313
 
 	defer func() {
 		os.RemoveAll(dir)
@@ -63,16 +65,115 @@ func TestServer(t *testing.T) {
 	// But for now, let us sleep and pray!
 	time.Sleep(2 * time.Second)
 
-	resp, err := http.Get("http://localhost:1331/")
-	c.Assert(err, qt.IsNil)
-	defer resp.Body.Close()
-	homeContent := helpers.ReaderToString(resp.Body)
-
-	c.Assert(homeContent, qt.Contains, "List: Hugo Commands")
-	c.Assert(homeContent, qt.Contains, "Environment: development")
+	checkHttpResp(c, t, "http://localhost:1313/", "List: Hugo Commands")
+	checkHttpResp(c, t, "http://localhost:1313/", "Environment: development")
 
 	// Stop the server.
 	stop <- true
+}
+
+func TestServerRenderStaticToDisk(t *testing.T) {
+	// Basically same as TestServer
+	if isWindowsCI() {
+		t.Skip("Skip server test on appveyor")
+	}
+	c := qt.New(t)
+	dir, clean, err := createSimpleTestSite(t, testSiteConfig{})
+
+	contentDir := "content"
+	staticDir := "static"
+	writeFile(t, filepath.Join(dir, contentDir, "content.csv"), "static data in contentDir")
+	writeFile(t, filepath.Join(dir, staticDir, "static.csv"), "static data in staticDir")
+	defer clean()
+	c.Assert(err, qt.IsNil)
+
+	// If server tests run in pararrel, port 1313 is blocked.
+	// Expect that this port is available.
+	port := 1333
+
+	defer func() {
+		os.RemoveAll(dir)
+	}()
+
+	stop := make(chan bool)
+
+	b := newCommandsBuilder()
+	scmd := b.newServerCmdSignaled(stop)
+
+	cmd := scmd.getCommand()
+	cmd.SetArgs([]string{"-s=" + dir, fmt.Sprintf("-p=%d", port), "--renderStaticToDisk"})
+
+	go func() {
+		_, err = cmd.ExecuteC()
+		c.Assert(err, qt.IsNil)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	checkHttpResp(c, t, "http://localhost:1333/", "List: Hugo Commands")
+	checkHttpResp(c, t, "http://localhost:1333/", "Environment: development")
+
+	// After inital build, in public,
+	// "static.csv" and "content.csv" exist
+	// "p1.md"-related html does NOT exist
+	files, err := ioutil.ReadDir(dir + "/public")
+	c.Assert(err, qt.IsNil)
+	for _, f := range files {
+		c.Assert([]string{"static.csv", "content.csv"}, qt.Any(qt.Equals), f.Name())
+	}
+
+	checkHttpResp(c, t, "http://localhost:1333/content.csv", "static data in contentDir")
+	checkHttpResp(c, t, "http://localhost:1333/static.csv", "static data in staticDir")
+
+	writeFile(t, filepath.Join(dir, contentDir, "content.csv"), "static data in contentDir can be updated")
+	writeFile(t, filepath.Join(dir, staticDir, "static.csv"), "static data in staticDir can be updated")
+
+	writeFile(t, filepath.Join(dir, contentDir, "added/content.csv"), "static data in contentDir can be added")
+	writeFile(t, filepath.Join(dir, staticDir, "added/static.csv"), "static data in staticDir can be added")
+
+	writeFile(t, filepath.Join(dir, contentDir, "conflicted.csv"), "if file name is conflicted, data in contentDir is served")
+	writeFile(t, filepath.Join(dir, staticDir, "conflicted.csv"), "if file name is conflicted, data in staticDir is served")
+
+	time.Sleep(2 * time.Second)
+
+	checkHttpResp(c, t, "http://localhost:1333/content.csv", "static data in contentDir can be updated")
+	checkHttpResp(c, t, "http://localhost:1333/static.csv", "static data in staticDir can be updated")
+
+	checkHttpResp(c, t, "http://localhost:1333/added/content.csv", "static data in contentDir can be added")
+	checkHttpResp(c, t, "http://localhost:1333/added/static.csv", "static data in staticDir can be added")
+
+	checkHttpResp(c, t, "http://localhost:1333/conflicted.csv", "if file name is conflicted, data in contentDir is served")
+
+	files, err = ioutil.ReadDir(dir + "/public")
+	c.Assert(err, qt.IsNil)
+	for _, f := range files {
+		c.Assert([]string{"static.csv", "content.csv", "conflicted.csv", "added"}, qt.Any(qt.Equals), f.Name())
+	}
+
+	// can be removed
+	os.RemoveAll(filepath.Join(dir, staticDir, "added"))
+	os.RemoveAll(filepath.Join(dir, contentDir, "added"))
+
+	time.Sleep(1 * time.Second)
+
+	checkHttpResp(c, t, "http://localhost:1333/added/content.csv", "404 page not found")
+	checkHttpResp(c, t, "http://localhost:1333/added/static.csv", "404 page not found")
+
+	files, err = ioutil.ReadDir(dir + "/public")
+	c.Assert(err, qt.IsNil)
+	for _, f := range files {
+		c.Assert([]string{"static.csv", "content.csv", "conflicted.csv"}, qt.Any(qt.Equals), f.Name())
+	}
+
+	stop <- true
+}
+
+func checkHttpResp(c *qt.C, t *testing.T, url string, part string) {
+	resp, err := http.Get(url)
+	c.Assert(err, qt.IsNil)
+	defer resp.Body.Close()
+	content := helpers.ReaderToString(resp.Body)
+	c.Assert(content, qt.Contains, part)
 }
 
 func TestFixURL(t *testing.T) {
