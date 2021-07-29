726fe9c3c97a9c979dc7862e7f226fc5ec1341de
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-07-29 16:18:38 +0200

    Go back to WARNING for Page deprecations
    Do that in the next release, let's reduce the noise, we have enough changes as it is.

--- resources/page/page_wrappers.autogen.go
+++ resources/page/page_wrappers.autogen.go
@@ -32,66 +32,66 @@ type pageDeprecated struct {
 }
 
 func (p *pageDeprecated) Filename() string {
-	helpers.Deprecated("Page.Filename", "Use .File.Filename", true)
+	helpers.Deprecated("Page.Filename", "Use .File.Filename", false)
 	return p.p.Filename()
 }
 func (p *pageDeprecated) Dir() string {
-	helpers.Deprecated("Page.Dir", "Use .File.Dir", true)
+	helpers.Deprecated("Page.Dir", "Use .File.Dir", false)
 	return p.p.Dir()
 }
 func (p *pageDeprecated) IsDraft() bool {
-	helpers.Deprecated("Page.IsDraft", "Use .Draft.", true)
+	helpers.Deprecated("Page.IsDraft", "Use .Draft.", false)
 	return p.p.IsDraft()
 }
 func (p *pageDeprecated) Extension() string {
-	helpers.Deprecated("Page.Extension", "Use .File.Extension", true)
+	helpers.Deprecated("Page.Extension", "Use .File.Extension", false)
 	return p.p.Extension()
 }
 func (p *pageDeprecated) Hugo() hugo.Info {
-	helpers.Deprecated("Page.Hugo", "Use the global hugo function.", true)
+	helpers.Deprecated("Page.Hugo", "Use the global hugo function.", false)
 	return p.p.Hugo()
 }
 func (p *pageDeprecated) Ext() string {
-	helpers.Deprecated("Page.Ext", "Use .File.Ext", true)
+	helpers.Deprecated("Page.Ext", "Use .File.Ext", false)
 	return p.p.Ext()
 }
 func (p *pageDeprecated) LanguagePrefix() string {
-	helpers.Deprecated("Page.LanguagePrefix", "Use .Site.LanguagePrefix.", true)
+	helpers.Deprecated("Page.LanguagePrefix", "Use .Site.LanguagePrefix.", false)
 	return p.p.LanguagePrefix()
 }
 func (p *pageDeprecated) GetParam(arg0 string) interface{} {
-	helpers.Deprecated("Page.GetParam", "Use .Param or .Params.myParam.", true)
+	helpers.Deprecated("Page.GetParam", "Use .Param or .Params.myParam.", false)
 	return p.p.GetParam(arg0)
 }
 func (p *pageDeprecated) LogicalName() string {
-	helpers.Deprecated("Page.LogicalName", "Use .File.LogicalName", true)
+	helpers.Deprecated("Page.LogicalName", "Use .File.LogicalName", false)
 	return p.p.LogicalName()
 }
 func (p *pageDeprecated) BaseFileName() string {
-	helpers.Deprecated("Page.BaseFileName", "Use .File.BaseFileName", true)
+	helpers.Deprecated("Page.BaseFileName", "Use .File.BaseFileName", false)
 	return p.p.BaseFileName()
 }
 func (p *pageDeprecated) RSSLink() template.URL {
-	helpers.Deprecated("Page.RSSLink", "Use the Output Format's link, e.g. something like:\n    {{ with .OutputFormats.Get \"RSS\" }}{{ .RelPermalink }}{{ end }}", true)
+	helpers.Deprecated("Page.RSSLink", "Use the Output Format's link, e.g. something like:\n    {{ with .OutputFormats.Get \"RSS\" }}{{ .RelPermalink }}{{ end }}", false)
 	return p.p.RSSLink()
 }
 func (p *pageDeprecated) TranslationBaseName() string {
-	helpers.Deprecated("Page.TranslationBaseName", "Use .File.TranslationBaseName", true)
+	helpers.Deprecated("Page.TranslationBaseName", "Use .File.TranslationBaseName", false)
 	return p.p.TranslationBaseName()
 }
 func (p *pageDeprecated) URL() string {
-	helpers.Deprecated("Page.URL", "Use .Permalink or .RelPermalink. If what you want is the front matter URL value, use .Params.url", true)
+	helpers.Deprecated("Page.URL", "Use .Permalink or .RelPermalink. If what you want is the front matter URL value, use .Params.url", false)
 	return p.p.URL()
 }
 func (p *pageDeprecated) ContentBaseName() string {
-	helpers.Deprecated("Page.ContentBaseName", "Use .File.ContentBaseName", true)
+	helpers.Deprecated("Page.ContentBaseName", "Use .File.ContentBaseName", false)
 	return p.p.ContentBaseName()
 }
 func (p *pageDeprecated) UniqueID() string {
-	helpers.Deprecated("Page.UniqueID", "Use .File.UniqueID", true)
+	helpers.Deprecated("Page.UniqueID", "Use .File.UniqueID", false)
 	return p.p.UniqueID()
 }
 func (p *pageDeprecated) FileInfo() hugofs.FileMetaInfo {
-	helpers.Deprecated("Page.FileInfo", "Use .File.FileInfo", true)
+	helpers.Deprecated("Page.FileInfo", "Use .File.FileInfo", false)
 	return p.p.FileInfo()
 }
