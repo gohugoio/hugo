32c6f656d93ecf4308f7c30848b13b4c6f157436
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-10-19 08:08:21 +0200

    create: Return error on no content dirs
    Closes #9056

--- create/content.go
+++ create/content.go
@@ -53,6 +53,9 @@ draft: true
 // NewContent creates a new content file in h (or a full bundle if the archetype is a directory)
 // in targetPath.
 func NewContent(h *hugolib.HugoSites, kind, targetPath string) error {
+	if h.BaseFs.Content.Dirs == nil {
+		return errors.New("no existing content directory configured for this project")
+	}
 	unlock, err := h.BaseFs.LockBuild()
 	if err != nil {
 		return fmt.Errorf("failed to acquire a build lock: %s", err)
