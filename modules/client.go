fc293b9f26d52b870af07fb2995cb7e77f2953c0
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2021-11-10 12:36:25 +0700

    Fix: typos and wording

--- modules/client.go
+++ modules/client.go
@@ -627,14 +627,14 @@ func (c *Client) runGo(
 
 		if strings.Contains(stderr.String(), "invalid version: unknown revision") {
 			// See https://github.com/gohugoio/hugo/issues/6825
-			c.logger.Println(`An unknown resivion most likely means that someone has deleted the remote ref (e.g. with a force push to GitHub).
+			c.logger.Println(`An unknown revision most likely means that someone has deleted the remote ref (e.g. with a force push to GitHub).
 To resolve this, you need to manually edit your go.mod file and replace the version for the module in question with a valid ref.
 
-The easiest is to just enter a valid branch name ther, e.g. master, which would be the what you put in place of 'v0.5.1' in the example below.
+The easiest is to just enter a valid branch name there, e.g. master, which would be what you put in place of 'v0.5.1' in the example below.
 
 require github.com/gohugoio/hugo-mod-jslibs/instantpage v0.5.1
 
-If you then run 'hugo mod graph' it should resolve itself to the most recent version (or commit if no semver versions).`)
+If you then run 'hugo mod graph' it should resolve itself to the most recent version (or commit if no semver versions are available).`)
 		}
 
 		_, ok := err.(*exec.ExitError)
