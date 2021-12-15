8ee6de6d96a64395d27416d4f2ad7d172a2686d0
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-12-15 18:00:51 +0100

    deps: Upgrade github.com/evanw/esbuild v0.14.2 => v0.14.5

--- hugolib/js_test.go
+++ hugolib/js_test.go
@@ -133,7 +133,7 @@ TS2: {{ template "print" $ts2 }}
 	b.AssertFileContent("public/index.html", `
 console.log(&#34;included&#34;);
 if (hasSpace.test(string))
-var React = __toModule(__require(&#34;react&#34;));
+var React = __toESM(__require(&#34;react&#34;));
 function greeter(person) {
 `)
 }
