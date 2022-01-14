bf8142efad1bf7eb4a5e5667586416a686aaeb61
Andreas Deininger <adeininger@gmail.com>
2022-01-14 23:07:28 +0100

    Fixing typos

--- modules/config.go
+++ modules/config.go
@@ -313,7 +313,7 @@ type HugoVersion struct {
 	// The minimum Hugo version that this module works with.
 	Min hugo.VersionString
 
-	// The maxium Hugo version that this module works with.
+	// The maximum Hugo version that this module works with.
 	Max hugo.VersionString
 
 	// Set if the extended version is needed.
@@ -365,7 +365,7 @@ func (v HugoVersion) IsValid() bool {
 type Import struct {
 	Path                string // Module path
 	pathProjectReplaced bool   // Set when Path is replaced in project config.
-	IgnoreConfig        bool   // Ignore any config in config.toml (will still folow imports).
+	IgnoreConfig        bool   // Ignore any config in config.toml (will still follow imports).
 	IgnoreImports       bool   // Do not follow any configured imports.
 	NoMounts            bool   // Do not mount any folder in this import.
 	NoVendor            bool   // Never vendor this import (only allowed in main project).
