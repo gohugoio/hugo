89c1655ec88cd7286c2ed5f9b7eaed35af9cda52
hugoreleaser <bjorn.erik.pedersen+hugoreleaser@gmail.com>
2022-04-28 11:02:45 +0000

    releaser: Prepare repository for 0.99.0-DEV
    [ci skip]

--- common/hugo/version_current.go
+++ common/hugo/version_current.go
@@ -16,7 +16,7 @@ package hugo
 // CurrentVersion represents the current build version.
 // This should be the only one.
 var CurrentVersion = Version{
-	Number:     0.98,
+	Number:     0.99,
 	PatchLevel: 0,
-	Suffix:     "",
+	Suffix:     "-DEV",
 }
