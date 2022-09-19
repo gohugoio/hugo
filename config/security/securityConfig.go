86653fa38eb43a5fabf7adc694c752ccb7721ffa
Mathieu Parent <math.parent@gmail.com>
2022-09-19 12:37:35 +0200

    config/security: Allow proxy variables in subcommands
    In particular for go get

--- config/security/securityConfig.go
+++ config/security/securityConfig.go
@@ -42,7 +42,7 @@ var DefaultConfig = Config{
 		),
 		// These have been tested to work with Hugo's external programs
 		// on Windows, Linux and MacOS.
-		OsEnv: NewWhitelist("(?i)^(PATH|PATHEXT|APPDATA|TMP|TEMP|TERM)$"),
+		OsEnv: NewWhitelist("(?i)^((HTTPS?|NO)_PROXY|PATH(EXT)?|APPDATA|TE?MP|TERM)$"),
 	},
 	Funcs: Funcs{
 		Getenv: NewWhitelist("^HUGO_"),
