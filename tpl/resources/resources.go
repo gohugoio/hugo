6df52cc4dcea933e395aca01fe25883f8fe9cd0e
shifterbit <48891590+shifterbit@users.noreply.github.com>
2022-11-15 00:22:38 -0500

    tlp/resources: resources.Get returns nil when given empty string

--- tpl/resources/resources.go
+++ tpl/resources/resources.go
@@ -123,7 +123,12 @@ func (ns *Namespace) Copy(s any, r resource.Resource) (resource.Resource, error)
 // Get locates the filename given in Hugo's assets filesystem
 // and creates a Resource object that can be used for further transformations.
 func (ns *Namespace) Get(filename any) resource.Resource {
+
 	filenamestr, err := cast.ToStringE(filename)
+
+	if filenamestr == "" {
+		return nil
+	}
 	if err != nil {
 		panic(err)
 	}
