dd6d0a6de16c871301d9daf5d749be3989d37526
Omar Kohl <omarkohl@posteo.net>
2023-01-03 20:09:03 +0100

    Remove reference to Goreleaser in code comment
    Hugo no longer uses the Goreleaser tool.

--- releaser/releaser.go
+++ releaser/releaser.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package releaser implements a set of utilities and a wrapper around Goreleaser
-// to help automate the Hugo release process.
+// Package releaser implements a set of utilities to help automate the
+// Hugo release process.
 package releaser
 
 import (
