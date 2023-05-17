5251f015bfff40b49f92f8855834d5682f925860
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-05-17 12:45:51 +0200

    Re-establish all the server flags
    Updates #10947

--- commands/commands.go
+++ commands/commands.go
@@ -20,6 +20,7 @@ import (
 // newExec wires up all of Hugo's CLI.
 func newExec() (*simplecobra.Exec, error) {
 	rootCmd := &rootCommand{
+		buildFlags: &buildFlags{},
 		commands: []simplecobra.Commander{
 			newVersionCmd(),
 			newEnvCommand(),
