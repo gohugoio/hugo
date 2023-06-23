5b4bfc2db719c1f282744c0c95dfba6c90f6eff2
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-06-23 12:09:57 +0200

    Fix broken nodeploy setup
    Fixes #11149

--- deploy/deployConfig.go
+++ deploy/deployConfig.go
@@ -11,9 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !nodeploy
-// +build !nodeploy
-
 package deploy
 
 import (
