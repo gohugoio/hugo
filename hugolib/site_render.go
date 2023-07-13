cc44583cc3976bf115c06474f47889d49842315e
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-07-13 15:42:30 +0200

    Improve behavior of defaultContentLanguageInSubdir when only the default language is enabled
    1 .Create sitemapindex in root of publishDir (will contain link to sitemap.xml in defaultContentLanguage site)
    2. Create index.html in root of publishDir (will redirect to defaultContentLanguage site)
    
    Fixes #11229

--- hugolib/site_render.go
+++ hugolib/site_render.go
@@ -384,7 +384,7 @@ func (s *Site) renderAliases() error {
 // renderMainLanguageRedirect creates a redirect to the main language home,
 // depending on if it lives in sub folder (e.g. /en) or not.
 func (s *Site) renderMainLanguageRedirect() error {
-	if !s.h.isMultiLingual() || s.h.Conf.IsMultihost() {
+	if s.h.Conf.IsMultihost() || !(s.h.Conf.DefaultContentLanguageInSubdir() || s.h.Conf.IsMultiLingual()) {
 		// No need for a redirect
 		return nil
 	}
