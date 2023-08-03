b10384213c674a99cb8b8621c75705115d0b9adf
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-08-03 17:52:17 +0200

    Add retry in resources.GetRemote for temporary HTTP errors
    Fixes #11312

--- resources/resource_factories/create/integration_test.go
+++ resources/resource_factories/create/integration_test.go
@@ -14,12 +14,17 @@
 package create_test
 
 import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gohugoio/hugo/hugolib"
 )
 
-func TestGetResourceHead(t *testing.T) {
+func TestGetRemoteHead(t *testing.T) {
 
 	files := `
 -- config.toml --
@@ -55,5 +60,65 @@ func TestGetResourceHead(t *testing.T) {
 		"Head Content: .",
 		"Head Data: map[ContentLength:18210 ContentType:image/png Status:200 OK StatusCode:200 TransferEncoding:[]]",
 	)
+
+}
+
+func TestGetRemoteRetry(t *testing.T) {
+	t.Parallel()
+
+	temporaryHTTPCodes := []int{408, 429, 500, 502, 503, 504}
+	numPages := 30
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		if rand.Intn(4) == 0 {
+			w.WriteHeader(temporaryHTTPCodes[rand.Intn(len(temporaryHTTPCodes))])
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		w.Write([]byte("Response for " + r.URL.Path + "."))
+	}
+
+	srv := httptest.NewServer(http.HandlerFunc(handler))
+	t.Cleanup(func() { srv.Close() })
+
+	files := `
+-- hugo.toml --
+disableKinds = ["home", "taxonomy", "term"]
+[security]
+[security.http]
+urls = ['.*']
+mediaTypes = ['text/plain']
+-- layouts/_default/single.html --
+{{ $url := printf "%s%s" "URL" .RelPermalink}}
+{{ $opts := dict }}
+{{ with resources.GetRemote $url $opts }}
+  {{ with .Err }}
+    {{ errorf "Unable to get remote resource: %s" . }}
+  {{ else }}
+    Content: {{ .Content }}
+  {{ end }}
+{{ else }}
+  {{ errorf "Unable to get remote resource: %s" $url }}
+{{ end }}
+`
+
+	for i := 0; i < numPages; i++ {
+		files += fmt.Sprintf("-- content/post/p%d.md --\n", i)
+	}
+
+	files = strings.ReplaceAll(files, "URL", srv.URL)
+
+	b := hugolib.NewIntegrationTestBuilder(
+		hugolib.IntegrationTestConfig{
+			T:           t,
+			TxtarString: files,
+		},
+	)
+
+	b.Build()
+
+	for i := 0; i < numPages; i++ {
+		b.AssertFileContent(fmt.Sprintf("public/post/p%d/index.html", i), fmt.Sprintf("Content: Response for /post/p%d/.", i))
+	}
 
 }
