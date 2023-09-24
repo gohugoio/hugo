d234a963eb4d4e713751f65bfb3a65e930400dbe
hugoreleaser <bjorn.erik.pedersen+hugoreleaser@gmail.com>
2023-09-24 15:32:33 +0000

    releaser: Prepare repository for 0.120.0-DEV
    [ci skip]

--- common/hugo/version_current.go
+++ common/hugo/version_current.go
@@ -17,7 +17,7 @@ package hugo
 // This should be the only one.
 var CurrentVersion = Version{
 	Major:      0,
-	Minor:      119,
+	Minor:      120,
 	PatchLevel: 0,
-	Suffix:     "",
+	Suffix:     "-DEV",
 }
