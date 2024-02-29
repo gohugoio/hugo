3f217fd6647b859b828b2ab1798177bebda64a9a
Joe Mooring <joe.mooring@veriphor.com>
2024-02-29 11:18:35 -0800

    resources/page: Make Taxonomy.Get and Taxonomy.Count case-insensitive
    Closes #12177

--- resources/page/taxonomy.go
+++ resources/page/taxonomy.go
@@ -16,6 +16,7 @@ package page
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/gohugoio/hugo/compare"
 	"github.com/gohugoio/hugo/langs"
@@ -58,11 +59,11 @@ type OrderedTaxonomyEntry struct {
 
 // Get the weighted pages for the given key.
 func (i Taxonomy) Get(key string) WeightedPages {
-	return i[key]
+	return i[strings.ToLower(key)]
 }
 
 // Count the weighted pages for the given key.
-func (i Taxonomy) Count(key string) int { return len(i[key]) }
+func (i Taxonomy) Count(key string) int { return len(i[strings.ToLower(key)]) }
 
 // TaxonomyArray returns an ordered taxonomy with a non defined order.
 func (i Taxonomy) TaxonomyArray() OrderedTaxonomy {
