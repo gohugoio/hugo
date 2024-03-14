48a0fea87afd706efbe3021c332444116af80c97
Joe Mooring <joe.mooring@veriphor.com>
2024-03-13 22:38:15 -0700

    tpl/tplimpl: Modify figure shortcode to look for page resource
    Closes #12244
    Closes #12245

--- hugolib/page_test.go
+++ hugolib/page_test.go
@@ -709,7 +709,7 @@ func TestPageWithShortCodeInSummary(t *testing.T) {
 	assertFunc := func(t *testing.T, ext string, pages page.Pages) {
 		p := pages[0]
 		checkPageTitle(t, p, "Simple")
-		checkPageContent(t, p, normalizeExpected(ext, "<p>Summary Next Line. <figure><img src=\"/not/real\"/> </figure> . More text here.</p><p>Some more text</p>"))
+		checkPageContent(t, p, normalizeExpected(ext, "<p>Summary Next Line. <figure><img src=\"/not/real\"> </figure> . More text here.</p><p>Some more text</p>"))
 		checkPageSummary(t, p, "Summary Next Line.  . More text here. Some more text")
 		checkPageType(t, p, "page")
 	}
