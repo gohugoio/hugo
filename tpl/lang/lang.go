6f07e5976de426c9637e77e3ae912df342c2b782
seiya <20365512+seiyab@users.noreply.github.com>
2024-04-03 09:12:13 +0900

    errors: Return error from cast.ToStringE() consistently

--- tpl/lang/lang.go
+++ tpl/lang/lang.go
@@ -58,7 +58,7 @@ func (ns *Namespace) Translate(ctx context.Context, id any, args ...any) (string
 
 	sid, err := cast.ToStringE(id)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 
 	return ns.deps.Translate(ctx, sid, templateData), nil
@@ -165,7 +165,7 @@ func (ns *Namespace) FormatNumberCustom(precision, number any, options ...any) (
 			// custom delimiter
 			s, err := cast.ToStringE(options[1])
 			if err != nil {
-				return "", nil
+				return "", err
 			}
 
 			delim = s
@@ -173,7 +173,7 @@ func (ns *Namespace) FormatNumberCustom(precision, number any, options ...any) (
 
 		s, err := cast.ToStringE(options[0])
 		if err != nil {
-			return "", nil
+			return "", err
 		}
 
 		rs := strings.Split(s, delim)
