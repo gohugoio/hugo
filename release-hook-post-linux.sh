46b437d5f8e892b37c047d51c5ef9ca39afc0271
broughtupsy <162842238+broughtupsy@users.noreply.github.com>
2024-04-15 18:47:22 +0300

    Update release-hook-post-linux.sh

--- release-hook-post-linux.sh
+++ release-hook-post-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Se https://github.com/gohugoio/hugo/issues/8955
+# See https://github.com/gohugoio/hugo/issues/8955
 objdump -T dist/hugo_extended_linux_linux_amd64/hugo | grep -E -q 'GLIBC_2.2[0-9]'
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
