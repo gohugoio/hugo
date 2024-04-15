72559f9dd2560b929212bdbdc9ade55298972230
broughtupsy <162842238+broughtupsy@users.noreply.github.com>
2024-04-15 18:44:42 +0300

    Update watchtestscripts.sh

--- watchtestscripts.sh
+++ watchtestscripts.sh
@@ -3,5 +3,5 @@
 trap exit SIGINT
 
 # I use "run tests on save" in my editor.
-# Unfortunately, changes to text files does not trigger this. Hence this workaround.
+# Unfortunately, changes to text files do not trigger this. Hence this workaround.
 while true; do find testscripts -type f -name "*.txt" | entr -pd touch main_test.go; done
