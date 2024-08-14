d72607adb9f17d9cca5f71bd80a81305f676da36
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-08-14 16:57:18 +0200

    markup/goldmark/blockquotes: Fix handling of lower/mixed case GitHub alerts
    Fixes #12767

--- markup/goldmark/blockquotes/blockquotes.go
+++ markup/goldmark/blockquotes/blockquotes.go
@@ -237,7 +237,8 @@ var _ hooks.PositionerSourceTargetProvider = (*blockquoteContext)(nil)
 // https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#alerts
 // Five types:
 // [!NOTE], [!TIP], [!WARNING], [!IMPORTANT], [!CAUTION]
-var gitHubAlertRe = regexp.MustCompile(`^<p>\[!(NOTE|TIP|WARNING|IMPORTANT|CAUTION)\]`)
+// Note that GitHub's implementation is case-insensitive.
+var gitHubAlertRe = regexp.MustCompile(`(?i)^<p>\[!(NOTE|TIP|WARNING|IMPORTANT|CAUTION)\]`)
 
 // resolveGitHubAlert returns one of note, tip, warning, important or caution.
 // An empty string if no match.
