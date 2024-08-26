f2049b9a13f33d3eca5dbfe53620b1358dbec4ac
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-08-26 12:39:07 +0200

    Adjust Circleci workflow to make the Docker images build
    Closes #12753

--- releaser/releaser.go
+++ releaser/releaser.go
@@ -101,7 +101,8 @@ func (r *ReleaseHandler) Run() error {
 			return err
 		}
 
-		if _, err := r.git("commit", "-a", "-m", fmt.Sprintf("%s Bump versions for release of %s\n\n[ci skip]", commitPrefix, newVersion)); err != nil {
+		// The reason we use NOCIRCLECI, see https://github.com/gohugoio/hugo/issues/12753
+		if _, err := r.git("commit", "-a", "-m", fmt.Sprintf("%s Bump versions for release of %s\n\nNOCIRCLECI", commitPrefix, newVersion)); err != nil {
 			return err
 		}
 
