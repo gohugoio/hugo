2f89169baa87a9db47e288b60705f4e99e21a945
hugoreleaser <bjorn.erik.pedersen+hugoreleaser@gmail.com>
2024-09-05 10:17:50 +0000

    releaser: Bump versions for release of 0.134.1
    [ci skip]

--- common/hugo/version_current.go
+++ common/hugo/version_current.go
@@ -17,7 +17,7 @@ package hugo
 // This should be the only one.
 var CurrentVersion = Version{
 	Major:      0,
-	Minor:      135,
-	PatchLevel: 0,
-	Suffix:     "-DEV",
+	Minor:      134,
+	PatchLevel: 1,
+	Suffix:     "",
 }
