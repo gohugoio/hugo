adc203c16e682143e581e3def641e8a5902b0245
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-09-30 09:11:24 +0200

    commands: Add "hugo build" as an alias for "hugo"
    Closes #11391

--- commands/commands.go
+++ commands/commands.go
@@ -14,6 +14,8 @@
 package commands
 
 import (
+	"context"
+
 	"github.com/bep/simplecobra"
 )
 
@@ -21,6 +23,7 @@ import (
 func newExec() (*simplecobra.Exec, error) {
 	rootCmd := &rootCommand{
 		commands: []simplecobra.Commander{
+			newHugoBuildCmd(),
 			newVersionCmd(),
 			newEnvCommand(),
 			newServerCommand(),
@@ -37,4 +40,34 @@ func newExec() (*simplecobra.Exec, error) {
 	}
 
 	return simplecobra.New(rootCmd)
+}
+
+func newHugoBuildCmd() simplecobra.Commander {
+	return &hugoBuildCommand{}
+}
+
+// hugoBuildCommand just delegates to the rootCommand.
+type hugoBuildCommand struct {
+	rootCmd *rootCommand
+}
+
+func (c *hugoBuildCommand) Commands() []simplecobra.Commander {
+	return nil
+}
+
+func (c *hugoBuildCommand) Name() string {
+	return "build"
+}
+
+func (c *hugoBuildCommand) Init(cd *simplecobra.Commandeer) error {
+	c.rootCmd = cd.Root.Command.(*rootCommand)
+	return c.rootCmd.initRootCommand("build", cd)
+}
+
+func (c *hugoBuildCommand) PreRun(cd, runner *simplecobra.Commandeer) error {
+	return c.rootCmd.PreRun(cd, runner)
+}
+
+func (c *hugoBuildCommand) Run(ctx context.Context, cd *simplecobra.Commandeer, args []string) error {
+	return c.rootCmd.Run(ctx, cd, args)
 }
