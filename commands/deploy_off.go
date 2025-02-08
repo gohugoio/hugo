4245a4514d58c0896900bc242390b31e92005d43
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-02-08 11:29:42 +0200

    all: Remove deprecated build tags

--- commands/deploy_off.go
+++ commands/deploy_off.go
@@ -12,7 +12,6 @@
 // limitations under the License.
 
 //go:build !withdeploy
-// +build !withdeploy
 
 // Copyright 2024 The Hugo Authors. All rights reserved.
 //
--- resources/resource_transformers/tocss/scss/client_notavailable.go
+++ resources/resource_transformers/tocss/scss/client_notavailable.go
@@ -12,7 +12,6 @@
 // limitations under the License.
 
 //go:build !extended
-// +build !extended
 
 package scss
 
