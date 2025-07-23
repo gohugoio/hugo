2b5b4641b9de4f2439d116ff8aca1dad8b6b2749
minxinyi <minxinyi6@outlook.com>
2025-07-23 15:07:33 +0800

    refactor: use slices.Equal to simplify code
    Signed-off-by: minxinyi <minxinyi6@outlook.com>

--- navigation/menu_cache.go
+++ navigation/menu_cache.go
@@ -28,7 +28,7 @@ func (entry menuCacheEntry) matches(menuList []Menu) bool {
 		return false
 	}
 	for i, m := range menuList {
-		if !menuEqual(m, entry.in[i]) {
+		if !slices.Equal(m, entry.in[i]) {
 			return false
 		}
 	}
@@ -44,21 +44,6 @@ func newMenuCache() *menuCache {
 type menuCache struct {
 	sync.RWMutex
 	m map[string][]menuCacheEntry
-}
-
-// menuEqual checks if two menus are equal.
-func menuEqual(m1, m2 Menu) bool {
-	if len(m1) != len(m2) {
-		return false
-	}
-
-	for i := range m1 {
-		if m1[i] != m2[i] {
-			return false
-		}
-	}
-
-	return true
 }
 
 // get retrieves a menu from the cache based on the provided key and menuLists.
