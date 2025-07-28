2eb3843994d8a6c29663f85e0f2f540791e378b8
Nigel van Keulen <nigel2392@gmail.com>
2025-07-28 12:51:16 +0200

    add even longer texts for Truncate benchmarks

--- tpl/strings/truncate_test.go
+++ tpl/strings/truncate_test.go
@@ -112,7 +112,7 @@ func BenchmarkTruncate(b *testing.B) {
 		}
 	})
 
-	b.Run("Plain text (long)", func(b *testing.B) {
+	b.Run("Plain text (medium)", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			ns.Truncate(371, `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -121,7 +121,7 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 		}
 	})
 
-	b.Run("With HTML (long)", func(b *testing.B) {
+	b.Run("With HTML (medium)", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			ns.Truncate(371, `Lorem ipsum dolor sit amet, <span>consectetur adipiscing elit</span>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, <p>quis nostrud exercitation ullamco</p> laboris nisi ut aliquip ex ea commodo consequat.
@@ -129,4 +129,45 @@ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
 Excepteur <code>sint occaecat cupidatat</code> non proident, <a href="#my-text">sunt in culpa qui officia deserunt mollit anim id est</a> laborum.`)
 		}
 	})
+
+	b.Run("Plain text (long)", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ns.Truncate(1850, `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.`)
+		}
+	})
+
+	b.Run("HTML text (long)", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ns.Truncate(1850, `Lorem ipsum dolor sit amet, <span>consectetur adipiscing elit</span>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, <p>quis nostrud exercitation ullamco</p> laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur <code>sint occaecat cupidatat</code> non proident, <a href="#my-text">sunt in culpa qui officia deserunt mollit anim id est</a> laborum.
+Sed ut <span>perspiciatis unde omnis iste natus error sit voluptatem</span> accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+Ut enim ad minima veniam, quis nostrum exercitationem <a href="/home-page">ullam corporis suscipit laboriosam</a>, nisi ut aliquid ex ea commodi consequatur?
+Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+occaecati cupiditate non provident, <div class="my-text-class">similique sunt in culpa qui officia deserunt mollitia animi</div>, id est laborum et dolorum fuga.
+Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe <h3>eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</h3>
+Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.`)
+		}
+	})
+
 }
