2b8805b4886fbc058ed35b678edee2f4ac4bed1d
Joe Mooring <joe.mooring@veriphor.com>
2025-10-24 12:11:54 -0700

    markup/asciidocext: Correct attribute derivation for workingFolderCurrent
    Fixes issues with `outdir`, `imagesoutdir`, and `imagesdir` when
    `markup.asciidocext.workingFolderCurrent` is active. The prior logic
    failed in several scenarios due to improper attribute derivation from the
    page's relative permalink.
    
    The updated logic now correctly handles:
    
    - Multi-byte characters
    - Multilingual multi-host sites
    - Site builds from a subdirectory
    - Pages using ugly URLs
    
    Closes #9202
    Closes #10183
    Closes #10473

--- markup/asciidocext/internal/converter.go
+++ markup/asciidocext/internal/converter.go
@@ -2,11 +2,16 @@ package internal
 
 import (
 	"bytes"
+	"fmt"
+	"net/url"
 	"path/filepath"
 	"strings"
 
 	"github.com/gohugoio/hugo/common/hexec"
+	"github.com/gohugoio/hugo/common/loggers"
+	"github.com/gohugoio/hugo/config/security"
 	"github.com/gohugoio/hugo/identity"
+	"github.com/gohugoio/hugo/langs"
 	"github.com/gohugoio/hugo/markup/asciidocext/asciidocext_config"
 	"github.com/gohugoio/hugo/markup/converter"
 	"github.com/gohugoio/hugo/markup/internal"
@@ -15,27 +20,28 @@ import (
 	"golang.org/x/net/html"
 )
 
-type AsciidocConverter struct {
+type AsciiDocConverter struct {
 	Ctx converter.DocumentContext
 	Cfg converter.ProviderConfig
 }
 
-type AsciidocResult struct {
+type AsciiDocResult struct {
 	converter.ResultRender
 	toc *tableofcontents.Fragments
 }
 
-/* ToDo: RelPermalink patch for svg posts not working*/
 type pageSubset interface {
+	IsPage() bool
 	RelPermalink() string
+	Section() string
 }
 
-func (r AsciidocResult) TableOfContents() *tableofcontents.Fragments {
+func (r AsciiDocResult) TableOfContents() *tableofcontents.Fragments {
 	return r.toc
 }
 
-func (a *AsciidocConverter) Convert(ctx converter.RenderContext) (converter.ResultRender, error) {
-	b, err := a.GetAsciidocContent(ctx.Src, a.Ctx)
+func (a *AsciiDocConverter) Convert(ctx converter.RenderContext) (converter.ResultRender, error) {
+	b, err := a.GetAsciiDocContent(ctx.Src, a.Ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -43,35 +49,37 @@ func (a *AsciidocConverter) Convert(ctx converter.RenderContext) (converter.Resu
 	if err != nil {
 		return nil, err
 	}
-	return AsciidocResult{
+	return AsciiDocResult{
 		ResultRender: converter.Bytes(content),
 		toc:          toc,
 	}, nil
 }
 
-func (a *AsciidocConverter) Supports(_ identity.Identity) bool {
+func (a *AsciiDocConverter) Supports(_ identity.Identity) bool {
 	return false
 }
 
-// GetAsciidocContent calls asciidoctor as an external helper
+// GetAsciiDocContent calls asciidoctor as an external helper
 // to convert AsciiDoc content to HTML.
-func (a *AsciidocConverter) GetAsciidocContent(src []byte, ctx converter.DocumentContext) ([]byte, error) {
-	if !HasAsciiDoc() {
-		a.Cfg.Logger.Errorln("asciidoctor not found in $PATH: Please install.\n",
-			"                 Leaving AsciiDoc content unrendered.")
+func (a *AsciiDocConverter) GetAsciiDocContent(src []byte, ctx converter.DocumentContext) ([]byte, error) {
+	if ok, err := HasAsciiDoc(); !ok {
+		a.Cfg.Logger.Errorf("%s: %s", err.Error(), "leaving AsciiDoc content unrendered")
 		return src, nil
 	}
 
-	args := a.ParseArgs(ctx)
+	args, err := a.ParseArgs(ctx)
+	if err != nil {
+		return nil, err
+	}
 	args = append(args, "-")
 
 	a.Cfg.Logger.Infoln("Rendering", ctx.DocumentName, " using asciidoctor args", args, "...")
 
-	return internal.ExternallyRenderContent(a.Cfg, ctx, src, asciiDocBinaryName, args)
+	return internal.ExternallyRenderContent(a.Cfg, ctx, src, AsciiDocBinaryName, args)
 }
 
-func (a *AsciidocConverter) ParseArgs(ctx converter.DocumentContext) []string {
-	cfg := a.Cfg.MarkupConfig().AsciidocExt
+func (a *AsciiDocConverter) ParseArgs(ctx converter.DocumentContext) ([]string, error) {
+	cfg := a.Cfg.MarkupConfig().AsciiDocExt
 	args := []string{}
 
 	args = a.AppendArg(args, "-b", cfg.Backend, asciidocext_config.CliDefault.Backend, asciidocext_config.AllowedBackend)
@@ -105,36 +113,57 @@ func (a *AsciidocConverter) ParseArgs(ctx converter.DocumentContext) []string {
 	}
 
 	if cfg.WorkingFolderCurrent {
-		contentDir := filepath.Dir(ctx.Filename)
-		destinationDir := a.Cfg.Conf.BaseConfig().PublishDir
-
-		if destinationDir == "" {
-			a.Cfg.Logger.Errorln("markup.asciidocext.workingFolderCurrent requires hugo command option --destination to be set")
+		page, ok := ctx.Document.(pageSubset)
+		if !ok {
+			return nil, fmt.Errorf("expected pageSubset, got %T", ctx.Document)
 		}
 
-		var outDir string
-		var err error
+		// Derive the outdir document attribute from the relative permalink.
+		relPath := strings.TrimPrefix(page.RelPermalink(), a.Cfg.Conf.BaseURL().BasePathNoTrailingSlash)
+		relPath, err := url.PathUnescape(relPath)
+		if err != nil {
+			return nil, err
+		}
 
-		file := filepath.Base(ctx.Filename)
-		if a.Cfg.Conf.IsUglyURLs("") || file == "_index.adoc" || file == "index.adoc" {
-			outDir, err = filepath.Abs(filepath.Dir(filepath.Join(destinationDir, ctx.DocumentName)))
-		} else {
-			postDir := ""
-			page, ok := ctx.Document.(pageSubset)
-			if ok {
-				postDir = filepath.Base(page.RelPermalink())
+		if a.Cfg.Conf.IsMultihost() {
+			// In a multi-host configuration, neither absolute nor relative
+			// permalinks include the language key; prepend it.
+			language, ok := a.Cfg.Conf.Language().(*langs.Language)
+			if !ok {
+				return nil, fmt.Errorf("expected *langs.Language, got %T", a.Cfg.Conf.Language())
+			}
+			relPath = filepath.Join(language.Lang, relPath)
+		}
+
+		if a.Cfg.Conf.IsUglyURLs(page.Section()) {
+			if page.IsPage() {
+				// Remove the extension.
+				relPath = strings.TrimSuffix(relPath, filepath.Ext(relPath))
 			} else {
-				a.Cfg.Logger.Errorln("unable to cast interface to pageSubset")
+				// Remove the file name.
+				relPath = filepath.Dir(relPath)
 			}
 
-			outDir, err = filepath.Abs(filepath.Join(destinationDir, filepath.Dir(ctx.DocumentName), postDir))
-		}
+			// Set imagesoutdir and imagesdir attributes.
+			imagesoutdir, err := filepath.Abs(filepath.Join(a.Cfg.Conf.BaseConfig().PublishDir, relPath))
+			if err != nil {
+				return nil, err
+			}
+			imagesdir := filepath.Base(imagesoutdir)
 
+			if page.IsPage() {
+				args = append(args, "-a", "imagesoutdir="+imagesoutdir, "-a", "imagesdir@="+imagesdir)
+			} else {
+				args = append(args, "-a", "imagesoutdir="+imagesoutdir)
+			}
+		}
+		// Prepend the publishDir.
+		outDir, err := filepath.Abs(filepath.Join(a.Cfg.Conf.BaseConfig().PublishDir, relPath))
 		if err != nil {
-			a.Cfg.Logger.Errorln("asciidoctor outDir: ", err)
+			return nil, err
 		}
 
-		args = append(args, "--base-dir", contentDir, "-a", "outdir="+outDir)
+		args = append(args, "--base-dir", filepath.Dir(ctx.Filename), "-a", "outdir="+outDir)
 	}
 
 	if cfg.NoHeaderOrFooter {
@@ -159,10 +188,10 @@ func (a *AsciidocConverter) ParseArgs(ctx converter.DocumentContext) []string {
 
 	args = a.AppendArg(args, "--safe-mode", cfg.SafeMode, asciidocext_config.CliDefault.SafeMode, asciidocext_config.AllowedSafeMode)
 
-	return args
+	return args, nil
 }
 
-func (a *AsciidocConverter) AppendArg(args []string, option, value, defaultValue string, allowedValues map[string]bool) []string {
+func (a *AsciiDocConverter) AppendArg(args []string, option, value, defaultValue string, allowedValues map[string]bool) []string {
 	if value != defaultValue {
 		if allowedValues[value] {
 			args = append(args, option, value)
@@ -173,15 +202,69 @@ func (a *AsciidocConverter) AppendArg(args []string, option, value, defaultValue
 	return args
 }
 
-const asciiDocBinaryName = "asciidoctor"
+const (
+	// AsciiDocBinaryName is the executable name for the AsciiDoc converter.
+	AsciiDocBinaryName = "asciidoctor"
+)
 
-func HasAsciiDoc() bool {
-	return hexec.InPath(asciiDocBinaryName)
+// HasAsciiDoc reports whether the AsciiDoc converter is installed.
+func HasAsciiDoc() (bool, error) {
+	if !hexec.InPath(AsciiDocBinaryName) {
+		return false, fmt.Errorf("the AsciiDoc converter (%s) is not installed", AsciiDocBinaryName)
+	}
+	return true, nil
+}
+
+// CanRenderDitaaDiagrams reports whether the AsciiDoc converter can render
+// Ditaa diagrams. Only used in tests.
+func CanRenderDitaaDiagrams() (bool, error) {
+	const (
+		// gemBinaryName is the executable name for the RubyGems CLI.
+		gemBinaryName = "gem"
+		// javaBinaryName is the executable name for the Java Runtime Environment CLI.
+		javaBinaryName = "java"
+	)
+
+	// Verify that the AsciiDoc converter is installed.
+	if ok, err := HasAsciiDoc(); !ok {
+		return false, err
+	}
+
+	// Verify that the RubyGems CLI is installed.
+	if !hexec.InPath(gemBinaryName) {
+		return false, fmt.Errorf("the RubyGems CLI (%s) is not installed", gemBinaryName)
+	}
+
+	// Verify that the required AsciiDoc converter extensions are installed.
+	extensions := []string{"asciidoctor-diagram", "asciidoctor-diagram-ditaamini"}
+
+	sc := security.DefaultConfig
+	sc.Exec.Allow = security.MustNewWhitelist(gemBinaryName)
+	ex := hexec.New(sc, "", loggers.NewDefault())
+
+	for _, extension := range extensions {
+		args := []any{"list", extension, "--installed"}
+		cmd, err := ex.New(gemBinaryName, args...)
+		if err != nil {
+			return false, err
+		}
+		err = cmd.Run()
+		if err != nil {
+			return false, fmt.Errorf("the %s gem is not installed", extension)
+		}
+	}
+
+	// Verify that the Java Runtime Environment CLI is installed.
+	if !hexec.InPath(javaBinaryName) {
+		return false, fmt.Errorf("the Java Runtime Environment CLI (%s) is not installed", javaBinaryName)
+	}
+
+	return true, nil
 }
 
 // extractTOC extracts the toc from the given src html.
 // It returns the html without the TOC, and the TOC data
-func (a *AsciidocConverter) extractTOC(src []byte) ([]byte, *tableofcontents.Fragments, error) {
+func (a *AsciiDocConverter) extractTOC(src []byte) ([]byte, *tableofcontents.Fragments, error) {
 	var buf bytes.Buffer
 	buf.Write(src)
 	node, err := html.Parse(&buf)
@@ -196,7 +279,7 @@ func (a *AsciidocConverter) extractTOC(src []byte) ([]byte, *tableofcontents.Fra
 	f = func(n *html.Node) bool {
 		if n.Type == html.ElementNode && n.Data == "div" && attr(n, "id") == "toc" {
 			toc = parseTOC(n)
-			if !a.Cfg.MarkupConfig().AsciidocExt.PreserveTOC {
+			if !a.Cfg.MarkupConfig().AsciiDocExt.PreserveTOC {
 				n.Parent.RemoveChild(n)
 			}
 			return true
