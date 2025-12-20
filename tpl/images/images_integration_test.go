d8b18ee0e551434a88df7de54b867a73361a9a31
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2025-12-20 15:03:10 +0100

    Handle PNG named *.webp
    Fixes #14288

--- tpl/images/images_integration_test.go
+++ tpl/images/images_integration_test.go
@@ -165,3 +165,26 @@ Brightnes method: /qr_hu_d5f06fd7594d0594.png
 Brightnes func: /qr_hu_d5f06fd7594d0594.png
 `)
 }
+
+func TestPNGNamedWebpIssue14288(t *testing.T) {
+	t.Parallel()
+
+	files := `
+-- assets/qr.webp --
+sourcefilename: testdata/images_golden/funcs/qr-level-high_scale-6.png
+-- layouts/home.html --
+{{ $img := resources.Get "qr.webp" }}
+images.Config: {{ (images.Config "assets/qr.webp").Width }}
+Resize to 100x100: {{ ($img.Resize "100x100" ).Width }}
+Brightnes method: {{ ($img.Filter (images.Brightness 12) ).RelPermalink }}
+Brightnes func: {{ ($img | images.Filter (images.Brightness 12) ).RelPermalink }}
+`
+
+	b := hugolib.Test(t, files)
+	b.AssertFileContent("public/index.html", `
+images.Config: 222
+Resize to 100x100: 100
+Brightnes method: /qr_hu_d5f06fd7594d0594.webp
+Brightnes func: /qr_hu_d5f06fd7594d0594.webp
+`)
+}
