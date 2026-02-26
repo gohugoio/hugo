e8577771c3275d16b7d1b31cca5e9fd3ea517416
Joe Mooring <joe.mooring@veriphor.com>
2026-02-26 11:08:08 -0800

    resources: Improve getImageOps error message
    Closes #14571

--- resources/transform.go
+++ resources/transform.go
@@ -395,12 +395,20 @@ func (r resourceAdapter) WithResourceMeta(mp resource.ResourceMetaProvider) reso
 func (r *resourceAdapter) getImageOps() images.ImageResourceOps {
 	img, ok := r.target.(images.ImageResourceOps)
 	if !ok {
-		if r.MediaType().SubType == "svg" {
-			panic("this method is only available for raster images. To determine if an image is SVG, you can do {{ if eq .MediaType.SubType \"svg\" }}{{ end }}")
-		}
-		panic("this method is only available for image resources")
+		instructions := "use reflect.IsImageResource, " +
+			"reflect.IsImageResourceProcessable, or " +
+			"reflect.IsImageResourceWithMeta to check if the resource " +
+			"supports this method before calling it"
+		msg := fmt.Sprintf(
+			"resource %q of media type %q does not support this method: %s",
+			r.Name(),
+			r.MediaType(),
+			instructions,
+		)
+		panic(msg)
 	}
 	r.init(false, false)
+
 	return img
 }
 
