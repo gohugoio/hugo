e8c98708af792eb99a928049d5e5b773f79d789f
Mira Sato <275437409+oab24413gmai@users.noreply.github.com>
2026-05-27 07:32:10 +0000

    fix: duplicated words in docshelper init-comments and chain.go transformer doc
    Signed-off-by: Mira Sato <275437409+oab24413gmai@users.noreply.github.com>

--- config/allconfig/docshelper.go
+++ config/allconfig/docshelper.go
@@ -19,7 +19,7 @@ import (
 	"github.com/gohugoio/hugo/docshelper"
 )
 
-// This is is just some helpers used to create some JSON used in the Hugo docs.
+// This is just some helpers used to create some JSON used in the Hugo docs.
 func init() {
 	docsProvider := func() docshelper.DocProvider {
 		cfg := config.New()
